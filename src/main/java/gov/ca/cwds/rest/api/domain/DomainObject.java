364f03324561e0af61fdfb47ff8d3e75fdf8fe47
Adarsh Vandana <adarsh.vandana@osi.ca.gov>
2017-02-07 10:59:26 -0800

    #138960329 fix for bug in ES Person

--- src/main/java/gov/ca/cwds/rest/api/domain/DomainObject.java
+++ src/main/java/gov/ca/cwds/rest/api/domain/DomainObject.java
@@ -1,9 +1,24 @@
 package gov.ca.cwds.rest.api.domain;
 
-public abstract class DomainObject {
+import java.io.Serializable;
+
+/**
+ * Base domain class for all API domain objects.
+ *
+ * @author CWDS API Team
+ */
+public abstract class DomainObject implements Serializable {
+
+  /**
+   * Base serialization version. Increment by class change.
+   */
+  private static final long serialVersionUID = 1L;
   public static final String DATE_FORMAT = DomainChef.DATE_FORMAT;
   public static final String TIME_FORMAT = DomainChef.TIME_FORMAT;
 
+  /**
+  *
+  */
   public DomainObject() {}
 
   @Override
