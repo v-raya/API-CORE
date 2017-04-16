69622ae6b6d2d19b5f1b85143d621bdb88bd30ca
David R. Smith <drs.code.monkey@gmail.com>
2017-04-15 20:37:47 -0700

    #140456485: address components

--- src/main/java/gov/ca/cwds/data/std/ApiAddressAware.java
+++ src/main/java/gov/ca/cwds/data/std/ApiAddressAware.java
@@ -53,4 +53,59 @@ public interface ApiAddressAware extends Serializable {
    */
   String getCounty();
 
+  /**
+   * Getter for optional zip 4.
+   * 
+   * @return zip4
+   */
+  default String getZip4() {
+    return null;
+  }
+
+  /**
+   * Getter for optional unit type, like "apt" or "suite".
+   * 
+   * @return optional unit type
+   */
+  default String getUnitType() {
+    return null;
+  }
+
+  /**
+   * Getter for optional unit number, like apartment number of suite number.
+   * 
+   * @return optional unit number
+   */
+  default String getUnitNumber() {
+    return null;
+  }
+
+  /**
+   * Getter for optional address type, such as home, foster, relative, etc. Not always available in
+   * legacy.
+   * 
+   * @return optional address type
+   */
+  default String getAddressType() {
+    return null;
+  }
+
+  /**
+   * Getter street name.
+   * 
+   * @return street name
+   */
+  default String getStreetName() {
+    return null;
+  }
+
+  /**
+   * Getter for street number.
+   * 
+   * @return street number
+   */
+  default String getStreetNumber() {
+    return null;
+  }
+
 }
