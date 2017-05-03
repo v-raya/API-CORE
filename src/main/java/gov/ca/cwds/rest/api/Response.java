e9cf833a6731b500d8eaea991b55b4d8364acbe2
David R. Smith <drs.code.monkey@gmail.com>
2017-05-02 19:50:55 -0700

    Response messages

--- src/main/java/gov/ca/cwds/rest/api/Response.java
+++ src/main/java/gov/ca/cwds/rest/api/Response.java
@@ -1,6 +1,7 @@
 package gov.ca.cwds.rest.api;
 
 import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
 
 /**
  * Marker indicating an object is used as a response from the API.
@@ -8,6 +9,24 @@ import java.util.Set;
  * @author CWDS API Team
  */
 public interface Response {
-    public boolean hasMessages();
-    public Set getMessages();
+
+  /**
+   * Tells whether Jersey response contains additional messages,
+   * 
+   * @return whether Jersey response contains additional messages
+   */
+  default boolean hasMessages() {
+    return false;
+  }
+
+  /**
+   * Returns Set of additional messages from Jersey response.
+   * 
+   * @return Set of messages or empty set if none
+   * @see #hasMessages()
+   */
+  default Set<String> getMessages() {
+    return new ConcurrentSkipListSet<>();
+  }
+
 }
