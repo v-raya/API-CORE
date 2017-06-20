747961c042cc6af1de62e335b2b5a5ba6d8ff7b8
David R. Smith <drs.code.monkey@gmail.com>
2017-06-20 15:11:31 -0700

    #147241475: DB2 referential integrity

---
+++ src/main/java/gov/ca/cwds/data/ApiReferentialIntegrityInterceptor.java
@@ -0,0 +1,76 @@
+package gov.ca.cwds.data;
+
+import java.io.Serializable;
+import java.util.Iterator;
+
+import org.hibernate.EmptyInterceptor;
+import org.hibernate.type.Type;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hibernate interceptor traps referential integrity errors.
+ * 
+ * @author CWDS API Team
+ */
+public class ApiReferentialIntegrityInterceptor extends EmptyInterceptor {
+
+  private static final long serialVersionUID = 1L;
+
+  private static final Logger LOGGER =
+      LoggerFactory.getLogger(ApiReferentialIntegrityInterceptor.class);
+
+  @Override
+  public void onDelete(Object entity, Serializable id, Object[] state, String[] propertyNames,
+      Type[] types) {
+    LOGGER.info("on delete");
+  }
+
+  // Called on entity update.
+  @Override
+  public boolean onFlushDirty(Object entity, Serializable id, Object[] currentState,
+      Object[] previousState, String[] propertyNames, Type[] types) {
+    LOGGER.info("on flush dirty");
+
+    // if (entity instanceof Student) {
+    // LOGGER.info("Student Update Operation");
+    // return true;
+    // }
+
+    return false;
+  }
+
+  // Called on entity load.
+  @Override
+  public boolean onLoad(Object entity, Serializable id, Object[] state, String[] propertyNames,
+      Type[] types) {
+    // log loading events
+    LOGGER.info("Load Operation");
+    return true;
+  }
+
+  @Override
+  public boolean onSave(Object entity, Serializable id, Object[] state, String[] propertyNames,
+      Type[] types) {
+    LOGGER.info("on save");
+
+    // if (entity instanceof Student) {
+    // LOGGER.info("Student Create Operation");
+    // return true;
+    // }
+
+    return false;
+  }
+
+  // Called before commit to database.
+  @Override
+  public void preFlush(Iterator iterator) {
+    LOGGER.info("Before commiting");
+  }
+
+  // Called after committed to database.
+  @Override
+  public void postFlush(Iterator iterator) {
+    LOGGER.info("After commiting");
+  }
+}
