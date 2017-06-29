137c0da1860f016d187ee8d307603c5d2d4dca36
David R. Smith <drs.code.monkey@gmail.com>
2017-06-29 13:43:58 -0700

    #147865633: request timestamp

--- src/test/java/gov/ca/cwds/rest/filters/ApiRequestCommonTest.java
+++ src/test/java/gov/ca/cwds/rest/filters/ApiRequestCommonTest.java
@@ -4,18 +4,25 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingDeque;
 
 import org.junit.Test;
 
 import gov.ca.cwds.auth.User;
 
 public class ApiRequestCommonTest {
+
+  protected LinkedBlockingDeque<Date> resultQueue = new LinkedBlockingDeque<>(10);
 
   @Test
   public void type() throws Exception {
@@ -32,27 +39,49 @@ public class ApiRequestCommonTest {
 
   @Test
   public void getRequestCommon_Thread() throws Exception {
-    ExecutorService service = Executors.newSingleThreadExecutor();
+    ExecutorService service = Executors.newFixedThreadPool(4);
+    Future<Date> future1 = execCallable(service, 5500);
+    Future<Date> future2 = execCallable(service, 3500);
+    Future<Date> future3 = execCallable(service, 5300);
+    Future<Date> future4 = execCallable(service, 2500);
 
-    Future<Date> future1 = execCallable(service, 1500);
-    Date result = null;
+    waitOnFuture(future1);
+    waitOnFuture(future2);
+    waitOnFuture(future3);
+    waitOnFuture(future4);
+
+    Set<Date> set = new HashSet<>();
+    for (Date dt : resultQueue) {
+      System.out.println("dt=" + new SimpleDateFormat("yyyy-MM-dd-HH.mm.ss.SSS").format(dt));
+      set.add(dt);
+    }
+
+    assertThat(set.size(), is(4));
+  }
+
+  protected void waitOnFuture(final Future<Date> future) {
     try {
-      result = future1.get();
+      Date result = future.get();
     } catch (InterruptedException e) {
       e.printStackTrace();
     } catch (ExecutionException e) {
       e.printStackTrace();
     }
-    System.out.println("date = " + result);
   }
 
   protected Future<Date> execCallable(ExecutorService service, int sleepMillis) {
     return service.submit(() -> {
+      final long tid = Thread.currentThread().getId();
       Thread.sleep(sleepMillis); // NOSONAR
-      User user_ = mock(User.class);
+      final User user_ = mock(User.class);
+      when(user_.getRacf()).thenReturn(tid + "_" + sleepMillis);
       ApiRequestCommon.pegRequest(user_);
       ApiRequestCommon common = ApiRequestCommon.getRequestCommon();
-      return common.getRequestBegin();
+      final Date date = common.getRequestBegin();
+      System.out.println("thread id: " + tid + ", racf: " + common.getUser().getRacf() + ", date = "
+          + new SimpleDateFormat("yyyy-MM-dd-HH.mm.ss.SSS").format(date));
+      resultQueue.add(date);
+      return date;
     });
   }
 
