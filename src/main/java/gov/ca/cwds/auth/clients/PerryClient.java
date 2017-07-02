b594213f11c334b0dda0c426e4cbb316ec5bb3df
David R. Smith <drs.code.monkey@gmail.com>
2017-07-02 12:45:07 -0700

    #145969351: system code validation

--- src/main/java/gov/ca/cwds/auth/clients/PerryClient.java
+++ src/main/java/gov/ca/cwds/auth/clients/PerryClient.java
@@ -23,8 +23,11 @@ import gov.ca.cwds.auth.InvalidTokenException;
 import gov.ca.cwds.auth.PerryShiroToken;
 
 public class PerryClient {
+
   private static final Logger LOGGER = LoggerFactory.getLogger(PerryClient.class);
+
   private volatile Client client;
+
   private String perryUrl;
   private String serviceProviderId;
 
@@ -43,32 +46,26 @@ public class PerryClient {
     return client;
   }
 
-  public String getServiceProviderId() {
-    return serviceProviderId;
-  }
-
-  public void setServiceProviderId(String serviceProviderId) {
-    this.serviceProviderId = serviceProviderId;
-  }
-
   public String validateToken(PerryShiroToken token) {
     checkNotNull(token, "Token cannot be null.");
     checkState(!Strings.isNullOrEmpty(perryUrl), "PerryUrl must be set.");
 
     LOGGER.debug("Requesting token validation on token: {}", token.getToken());
     Response response = null;
+
     try {
       WebTarget webTarget = getClient().target(perryUrl).queryParam("token", token.getToken());
-      if(serviceProviderId != null) {
+      if (serviceProviderId != null) {
         webTarget = webTarget.queryParam("sp_id", serviceProviderId);
       }
+
       Invocation.Builder invocationBuilder = webTarget.request(MediaType.TEXT_PLAIN_TYPE);
       response = invocationBuilder.get();
     } catch (Exception e) {
       LOGGER.error("Error validating a token.", e);
       throw new ApiAuthenticationException("Error validating a token.", e);
-
     }
+
     if (response != null) {
       if (response.getStatus() == 200) {
         String subject = response.readEntity(String.class);
@@ -86,11 +83,22 @@ public class PerryClient {
           MessageFormat.format("Null response validating token: {0}", token.getToken()));
     }
 
-
   }
 
+  @SuppressWarnings("javadoc")
   public void setPerryUrl(String perryUrl) {
     checkArgument(!Strings.isNullOrEmpty(perryUrl), "PerryUrl cannot be null or empty.");
     this.perryUrl = perryUrl;
   }
+
+  @SuppressWarnings("javadoc")
+  public String getServiceProviderId() {
+    return serviceProviderId;
+  }
+
+  @SuppressWarnings("javadoc")
+  public void setServiceProviderId(String serviceProviderId) {
+    this.serviceProviderId = serviceProviderId;
+  }
+
 }
---
+++ src/test/java/gov/ca/cwds/auth/clients/PerryClientTest.java
@@ -0,0 +1,63 @@
+package gov.ca.cwds.auth.clients;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+
+import gov.ca.cwds.auth.PerryShiroToken;
+
+public class PerryClientTest {
+
+  @Test
+  public void type() throws Exception {
+    assertThat(PerryClient.class, notNullValue());
+  }
+
+  @Test
+  public void instantiation() throws Exception {
+    PerryClient target = new PerryClient();
+    assertThat(target, notNullValue());
+  }
+
+  @Test
+  public void validateToken_Args__PerryShiroToken() throws Exception {
+    PerryClient target = new PerryClient();
+    String perryUrl = "http://localhost:8082/perry";
+    target.setPerryUrl(perryUrl);
+
+    PerryShiroToken token = mock(PerryShiroToken.class);
+    when(token.getToken()).thenReturn("38e7djdslshhdkd");
+
+    String actual = target.validateToken(token);
+    String expected = null;
+    assertThat(actual, is(equalTo(expected)));
+  }
+
+  @Test
+  public void setPerryUrl_Args__String() throws Exception {
+    PerryClient target = new PerryClient();
+    String perryUrl = "http://localhost:8082/perry";
+    target.setPerryUrl(perryUrl);
+  }
+
+  @Test
+  public void getServiceProviderId_Args__() throws Exception {
+    PerryClient target = new PerryClient();
+    String actual = target.getServiceProviderId();
+    String expected = null;
+    assertThat(actual, is(equalTo(expected)));
+  }
+
+  @Test
+  public void setServiceProviderId_Args__String() throws Exception {
+    PerryClient target = new PerryClient();
+    String serviceProviderId = null;
+    target.setServiceProviderId(serviceProviderId);
+  }
+
+}
