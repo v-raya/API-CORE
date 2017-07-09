4d92b1d29d6f77e14a4975df5367d903edbc7cc3
Shahid Saleemi <shahid.saleemi@doj.ca.gov>
2017-07-08 18:03:00 -0700

    Create index with provided settings

--- src/main/java/gov/ca/cwds/data/es/ElasticsearchDao.java
+++ src/main/java/gov/ca/cwds/data/es/ElasticsearchDao.java
@@ -16,6 +16,7 @@ import org.apache.commons.compress.utils.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.elasticsearch.action.ActionRequest;
 import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
+import org.elasticsearch.action.admin.indices.create.CreateIndexRequestBuilder;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.update.UpdateRequest;
@@ -23,6 +24,7 @@ import org.elasticsearch.client.Client;
 import org.elasticsearch.client.Requests;
 import org.elasticsearch.cluster.metadata.MetaData;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.indices.InvalidIndexNameException;
@@ -123,6 +125,74 @@ public class ElasticsearchDao implements Closeable {
   }
 
   /**
+   * Create ES index based on supplied parameters.
+   * 
+   * <p>
+   * This method intentionally synchronizes against race conditions by multiple, simultaneous
+   * attempts to create the same index.
+   * </p>
+   * 
+   * @param index Index name
+   * @param type Index document type
+   * @param settingsJsonFile Setting file
+   * @param mappingJsonFile Mapping file
+   * @throws IOException
+   */
+  private void createIndex(final String index, final String type, final String settingsJsonFile,
+      final String mappingJsonFile) throws IOException {
+    LOGGER.warn("CREATING ES INDEX [{}] for type [{}] with settings [{}] and mappings [{}]...",
+        index, type, settingsJsonFile, mappingJsonFile);
+
+    final String settingsSource = readFile(settingsJsonFile);
+    final String mappingSource = readFile(mappingJsonFile);
+
+    CreateIndexRequestBuilder createIndexRequestBuilder =
+        getClient().admin().indices().prepareCreate(index);
+
+    createIndexRequestBuilder.setSettings(settingsSource, XContentType.JSON);
+    createIndexRequestBuilder.addMapping(type, mappingSource, XContentType.JSON);
+
+    CreateIndexRequest indexRequest = createIndexRequestBuilder.request();
+    getClient().admin().indices().create(indexRequest).actionGet();
+  }
+
+  /**
+   * Create an index, if needed, before blasting documents into it.
+   * 
+   * <p>
+   * Defaults to 5 shards and 1 replica.
+   * </p>
+   * 
+   * <p>
+   * This method intentionally synchronizes against race conditions by multiple, simultaneous
+   * attempts to create the same index.
+   * </p>
+   * 
+   * @param index index name or alias
+   * @param optShards optional number of shards. Defaults to 5.
+   * @param optReplicas optional number of replicas. Defaults to 1.
+   * @throws IOException on disconnect, hang, etc.
+   */
+  public synchronized void createIndexIfNeeded(final String index, final String type,
+      final String settingsJsonFile, final String mappingJsonFile) throws IOException {
+
+    if (!doesIndexExist(index)) {
+      LOGGER.warn("ES INDEX {} DOES NOT EXIST!", index);
+      createIndex(index, type, settingsJsonFile, mappingJsonFile);
+
+      // Let Elasticsearch catch its breath.
+      try {
+        Thread.sleep(2000); // NOSONAR
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+        LOGGER.warn("Interrupted!");
+      }
+    } else {
+      LOGGER.warn("INDEX {} already exists!", index);
+    }
+  }
+
+  /**
    * Create an index and apply a mapping before blasting documents into it.
    * 
    * @param index index name or alias
@@ -130,7 +200,8 @@ public class ElasticsearchDao implements Closeable {
    * @param numReplicas number of replicas
    * @throws IOException on disconnect, hang, etc.
    */
-  public void createIndex(final String index, int numShards, int numReplicas) throws IOException {
+  @Deprecated
+  private void createIndex(final String index, int numShards, int numReplicas) throws IOException {
     LOGGER.warn("CREATE ES INDEX {} with {} shards and {} replicas", index, numShards, numReplicas);
 
     try {
@@ -172,7 +243,8 @@ public class ElasticsearchDao implements Closeable {
    * @param optReplicas optional number of replicas. Defaults to 1.
    * @throws IOException on disconnect, hang, etc.
    */
-  public synchronized void createIndexIfNeeded(final String index,
+  @Deprecated
+  private synchronized void createIndexIfNeeded(final String index,
       final Optional<Integer> optShards, final Optional<Integer> optReplicas) throws IOException {
 
     final int shards = optShards.orElse(5);
@@ -203,6 +275,7 @@ public class ElasticsearchDao implements Closeable {
    * @throws IOException on disconnect, hang, etc.
    * @see #createIndexIfNeeded(String, Optional, Optional)
    */
+  @Deprecated
   public synchronized void createIndexIfNeeded(final String index) throws IOException {
     createIndexIfNeeded(index, Optional.<Integer>empty(), Optional.<Integer>empty());
   }
@@ -336,8 +409,8 @@ public class ElasticsearchDao implements Closeable {
    * 
    * <p>
    * This method calls Elasticsearch's <a href=
-   * "https://www.elastic.co/guide/en/elasticsearch/guide/current/_best_fields.html#dis-max-query"
-   * >"dis max"</a> query feature.
+   * "https://www.elastic.co/guide/en/elasticsearch/guide/current/_best_fields.html#dis-max-query" >
+   * "dis max"</a> query feature.
    * </p>
    * 
    * @param searchTerm ES search String
@@ -570,4 +643,10 @@ public class ElasticsearchDao implements Closeable {
     return jsonString.toString();
   }
 
+  private String readFile(String sourceFile) throws IOException {
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    IOUtils.copy(this.getClass().getResourceAsStream(sourceFile), out);
+    out.flush();
+    return out.toString();
+  }
 }
