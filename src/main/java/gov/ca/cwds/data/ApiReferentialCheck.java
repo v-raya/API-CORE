ba60de9ebb8ab78c2889514cde69a05a6d233cb0
David R. Smith <drs.code.monkey@gmail.com>
2017-07-30 11:56:52 -0700

    #147241489: referential integrity

--- src/main/java/gov/ca/cwds/data/ApiReferentialCheck.java
+++ src/main/java/gov/ca/cwds/data/ApiReferentialCheck.java
@@ -6,10 +6,12 @@ import gov.ca.cwds.data.persistence.PersistentObject;
 import gov.ca.cwds.data.std.ApiMarker;
 
 /**
- * Validate foreign key relationships or other referential integrity.
+ * Validate foreign key relationships or other referential integrity. Trap referential integrity
+ * (RI) errors as a <strong>last resort</strong>.
  * 
  * @author CWDS API Team
  * @param <T> persistent object
+ * @see ApiHibernateInterceptor
  */
 @FunctionalInterface
 public interface ApiReferentialCheck<T extends PersistentObject>
