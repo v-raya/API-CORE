8785e8e9e488af49d18ec83bb7963e65b2e153e8
Ievgenii Drozd <justajavaboy@users.noreply.github.com>
2017-10-13 13:13:47 -0700

    151534487 ABAC: implement rules for Client county determination added additional test for county determination service

--- api-core-abac/src/test/java/gov/ca/cwds/service/ClientCountyDeterminationServiceImplTest.java
+++ api-core-abac/src/test/java/gov/ca/cwds/service/ClientCountyDeterminationServiceImplTest.java
@@ -3,7 +3,7 @@ package gov.ca.cwds.service;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.doReturn;
 
@@ -59,14 +59,83 @@ public class ClientCountyDeterminationServiceImplTest {
   }
 
   @Test
-  public void testHandleRequest() throws Exception {
+  public void testGetClientByClientAnyActiveCase() throws Exception {
     final List<Short> list = new ArrayList<>();
     list.add((short) 1);
+    doReturn(list).when(countyDeterminationDao).getClientCountyByActiveCase(Mockito.anyString());
+    Whitebox.setInternalState(countyDeterminationService, "countyDeterminationDao",
+        countyDeterminationDao);
+
+    assertThat(countyDeterminationService.getClientCountyById("testClientId"),
+        is(equalTo((short) 1)));
+  }
+
+  @Test
+  public void testGetCountyByActiveReferrals() throws Exception {
+    final List<Short> list = new ArrayList<>();
+    list.add((short) 2);
+    doReturn(list).when(countyDeterminationDao).getCountyByActiveReferrals(Mockito.anyString());
+    Whitebox.setInternalState(countyDeterminationService, "countyDeterminationDao",
+        countyDeterminationDao);
+
+    assertThat(countyDeterminationService.getClientCountyById("testClientId"),
+        is(equalTo((short) 2)));
+  }
+
+  @Test
+  public void testGetClientCountyByClosedCase() throws Exception {
+    final List<Short> list = new ArrayList<>();
+    list.add((short) 31);
+    doReturn(list).when(countyDeterminationDao).getClientCountyByClosedCase(Mockito.anyString());
+    Whitebox.setInternalState(countyDeterminationService, "countyDeterminationDao",
+        countyDeterminationDao);
+
+    assertThat(countyDeterminationService.getClientCountyById("testClientId"),
+        is(equalTo((short) 31)));
+  }
+
+  @Test
+  public void testGetClientCountyByClosedReferral() throws Exception {
+    final List<Short> list = new ArrayList<>();
+    list.add((short) 32);
+    doReturn(list).when(countyDeterminationDao)
+        .getClientCountyByClosedReferral(Mockito.anyString());
+    Whitebox.setInternalState(countyDeterminationService, "countyDeterminationDao",
+        countyDeterminationDao);
+
+    assertThat(countyDeterminationService.getClientCountyById("testClientId"),
+        is(equalTo((short) 32)));
+  }
+
+  @Test
+  public void testGetClientCountyByClientAnyActiveCase() throws Exception {
+    final List<Short> list = new ArrayList<>();
+    list.add((short) 4);
     doReturn(list).when(countyDeterminationDao).getClientByClientAnyActiveCase(Mockito.anyString());
     Whitebox.setInternalState(countyDeterminationService, "countyDeterminationDao",
         countyDeterminationDao);
 
-    assertThat(countyDeterminationService.getClientCountyById("testClientId"), is(equalTo((short)1)));
+    assertThat(countyDeterminationService.getClientCountyById("testClientId"),
+        is(equalTo((short) 4)));
+  }
 
+  @Test
+  public void testGetClientByClientAnyClosedCase() throws Exception {
+    final List<Short> list = new ArrayList<>();
+    list.add((short) 5);
+    doReturn(list).when(countyDeterminationDao).getClientByClientAnyClosedCase(Mockito.anyString());
+    Whitebox.setInternalState(countyDeterminationService, "countyDeterminationDao",
+        countyDeterminationDao);
+
+    assertThat(countyDeterminationService.getClientCountyById("testClientId"),
+        is(equalTo((short) 5)));
+  }
+
+  @Test
+  public void testNullClientCounty() throws Exception {
+    Whitebox.setInternalState(countyDeterminationService, "countyDeterminationDao",
+        countyDeterminationDao);
+
+    assertNull(countyDeterminationService.getClientCountyById("testClientId"));
   }
 }
