7113b9ec60cc2529a8e654086eaa728f4e3d862d
Ievgenii Drozd <justajavaboy@users.noreply.github.com>
2017-10-13 16:38:37 -0700

    151534487 ABAC: implement rules for Client county determination added additional tests for county determination DAO;

--- api-core-abac/src/test/java/gov/ca/cwds/data/dao/cms/CountyDeterminationDaoTest.java
+++ api-core-abac/src/test/java/gov/ca/cwds/data/dao/cms/CountyDeterminationDaoTest.java
@@ -1,15 +1,22 @@
 package gov.ca.cwds.data.dao.cms;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.hamcrest.junit.ExpectedException;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.query.NativeQuery;
-import org.hibernate.query.Query;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -52,15 +59,41 @@ public class CountyDeterminationDaoTest {
 
   @Test
   public void testGetClientByClientAnyActiveCase() throws Exception {
-    Session session = mock(Session.class);
+    List<Short> resultList = new ArrayList<>();
+    resultList.add((short) 11);
 
+    Session session = mock(Session.class);
+    Transaction transaction = mock(Transaction.class);
     NativeQuery query = mock(NativeQuery.class);
 
+    doReturn(resultList).when(query).getResultList();
+    doNothing().when(transaction).commit();
+    doReturn(true).when(transaction).isActive();
+    doNothing().when(transaction).rollback();
+    doReturn(transaction).when(session).getTransaction();
     doReturn(query).when(session).createNativeQuery(Mockito.anyString());
     doReturn(session).when(sessionFactory).getCurrentSession();
+    doReturn(query).when(query).setParameter(Mockito.anyString(), Mockito.anyString());
     Whitebox.setInternalState(countyDeterminationDao, "sessionFactory",
         sessionFactory);
 
+    assertThat(countyDeterminationDao.getClientCountyByActiveCase("testClientId").get(0),
+        is(equalTo((short) 11)));
 
+  }
+
+  @Test
+  public void testSessionException() throws Exception {
+    Session session = mock(Session.class);
+
+    doThrow(HibernateException.class).when(sessionFactory).getCurrentSession();
+    doReturn(session).when(sessionFactory).openSession();
+    doThrow(Exception.class).when(session).getTransaction();
+
+    Whitebox.setInternalState(countyDeterminationDao, "sessionFactory",
+        sessionFactory);
+
+    thrown.expect(Exception.class);
+    countyDeterminationDao.getClientCountyByActiveCase("testClientId");
   }
 }
