c292370d660a222563cc82d1ef2f0de708dcff6a
David R. Smith <David.Smith@osi.ca.gov>
2017-10-16 17:52:20 -0700

    Java CMS key generation

--- api-core-cms/src/main/java/gov/ca/cwds/data/persistence/cms/JavaKeyCmdLine.java
+++ api-core-cms/src/main/java/gov/ca/cwds/data/persistence/cms/JavaKeyCmdLine.java
@@ -1,8 +1,18 @@
 package gov.ca.cwds.data.persistence.cms;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Date;
+import java.util.stream.Stream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import gov.ca.cwds.rest.api.domain.DomainChef;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
 
 /**
  * Command line tool to call the <strong>Java</strong> CWDS key generation class,
@@ -21,18 +31,49 @@ import gov.ca.cwds.rest.api.domain.DomainChef;
  */
 public final class JavaKeyCmdLine {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(JavaKeyCmdLine.class);
+
+  private void massTest(String staffId, String fileNm) throws IOException {
+    final Path pathIn = Paths.get(fileNm);
+
+    try (Stream<String> lines = Files.lines(pathIn)) {
+      lines.forEach(l -> generateKey(staffId, l));
+    } finally {
+      // Close stream.
+    }
+  }
+
+  private void generateKey(String staffId, Date ts) throws IOException {
+    LOGGER.info("Java generateKey: staff: {}, timestamp: {}", staffId, ts);
+    final String key = CmsKeyIdGenerator.generate(staffId, ts);
+    LOGGER.info("Generated key=" + key);
+  }
+
+  private void generateKey(String staffId, String strTs) {
+    final Date ts = DomainChef.uncookISO8601Timestamp(strTs);
+    final String key = CmsKeyIdGenerator.generate(staffId, ts);
+    LOGGER.info("gen: staff: {}, timestamp: {}, key: {}", staffId, ts, key);
+  }
+
   /**
    * Main method. See class javadoc for details.
    * 
    * @param args command line
    */
   public static void main(String[] args) {
-    final String staffId = args[0];
-    final Date ts = DomainChef.uncookISO8601Timestamp(args[1]);
+    JavaKeyCmdLine run = new JavaKeyCmdLine();
+    try {
+      final OptionParser parser = new OptionParser("f:s:t:T::");
+      final OptionSet options = parser.parse(args);
 
-    System.out.println("Java: Call JNI generateKey ... ");
-    final String key = CmsKeyIdGenerator.generate(staffId, ts);
-    System.out.println("Generated key=" + key);
+      final String staffId = options.has("s") ? (String) options.valueOf("s") : "0x5";
+      final Date ts = options.has("t")
+          ? DomainChef.uncookISO8601Timestamp((String) options.valueOf("t")) : new Date();
+
+      run.generateKey(staffId, ts);
+    } catch (Exception e) {
+      LOGGER.error("OOPS!", e);
+    }
   }
 
 }
