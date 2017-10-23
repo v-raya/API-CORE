edc2ecb0556b6486463a238e5e7d7609c5f8c7c9
David R. Smith <David.Smith@osi.ca.gov>
2017-10-23 07:36:18 -0700

    #152121892: Neutron latency

--- api-core-cms/src/main/java/gov/ca/cwds/common/ApiFileAssistant.java
+++ api-core-cms/src/main/java/gov/ca/cwds/common/ApiFileAssistant.java
@@ -1,11 +1,15 @@
 package gov.ca.cwds.common;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import gov.ca.cwds.data.std.ApiMarker;
+import gov.ca.cwds.rest.api.ApiException;
 
 public class ApiFileAssistant implements ApiMarker {
 
@@ -20,6 +24,32 @@ public class ApiFileAssistant implements ApiMarker {
     IOUtils.copy(this.getClass().getResourceAsStream(sourceFile), out);
     out.flush();
     return out.toString();
+  }
+
+  /**
+   * Deter path traversal vulnerabilities.
+   * 
+   * @param loc proposed file location
+   * @return file to proposed location
+   */
+  public File validateFileLocation(String loc) {
+    File ret = null;
+    if (StringUtils.isNotBlank(loc)) {
+      final String cleanLoc =
+          loc.lastIndexOf('/') > -1 ? loc.substring(0, loc.lastIndexOf('/')) : loc;
+      if (cleanLoc.equals(FilenameUtils.normalize(cleanLoc))) {
+        ret = new File(cleanLoc);
+        if (ret.exists()) {
+          return ret;
+        }
+      }
+    }
+
+    if (ret != null) {
+      return ret;
+    } else {
+      throw new ApiException("PROHIBITED FILE LOCATION!");
+    }
   }
 
 }
