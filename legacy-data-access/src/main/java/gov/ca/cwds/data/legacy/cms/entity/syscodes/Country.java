7fc6e3f7f3aed46abda2eb84ffe172398395300c
dmytro.palczewski <dmytro.palczewski@engagepoint.com>
2017-11-09 10:38:07 -0800

    Country entity and CountryDao are added

---
+++ legacy-data-access/src/main/java/gov/ca/cwds/data/legacy/cms/entity/syscodes/Country.java
@@ -0,0 +1,21 @@
+package gov.ca.cwds.data.legacy.cms.entity.syscodes;
+
+import static gov.ca.cwds.data.legacy.cms.entity.syscodes.County.NQ_ALL;
+
+import javax.persistence.Cacheable;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import org.hibernate.annotations.NamedQuery;
+
+/** @author CWDS CASE API Team */
+@NamedQuery(name = NQ_ALL, query = "FROM Country ORDER BY lgcId ASC")
+@NamedQuery(name = "Country.findByLogicalId", query = "FROM Country where lgcId = :logicalId")
+@Entity
+@Cacheable
+@DiscriminatorValue(value = "CNTRY_C")
+public class Country extends SystemCodeTable {
+
+  private static final long serialVersionUID = 1L;
+
+  public static final String NQ_ALL = "Country.all";
+}
