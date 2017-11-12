79432384f259311d99b2f898733cf4cf06bcc75a
dmytro.palczewski <dmytro.palczewski@engagepoint.com>
2017-11-12 10:12:32 -0800

    LimitedAccess is refactored, description property is added

--- legacy-data-access/src/main/java/gov/ca/cwds/data/legacy/cms/entity/enums/LimitedAccess.java
+++ legacy-data-access/src/main/java/gov/ca/cwds/data/legacy/cms/entity/enums/LimitedAccess.java
@@ -1,34 +1,51 @@
 package gov.ca.cwds.data.legacy.cms.entity.enums;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author CWDS CASE API Team
  */
 public enum LimitedAccess {
-  NO_RESTRICTION("N/A"),
-  SEALED("R"),
-  SENSITIVE("S");
+  NO_RESTRICTION("N/A",  " N/A"),
+  SEALED("R", "Sealed"),
+  SENSITIVE("S", "Sensitive");
+
+  private static final Map<String, LimitedAccess> codeMap =
+      Collections.unmodifiableMap(initializeMapping());
 
   private final String code;
+  private final String description;
 
-  LimitedAccess(String code) {
+  LimitedAccess(String code, String description) {
     this.code = code;
+    this.description = description;
   }
 
   public static LimitedAccess fromCode(String code) {
-    if ("N/A".equals(code)) {
-      return NO_RESTRICTION;
-    } else if ("R".equals(code)) {
-      return SEALED;
-    } else if ("S".equals(code)) {
-      return SENSITIVE;
+    LimitedAccess result = codeMap.get(code);
+
+    if (result == null) {
+      throw new UnsupportedOperationException("The code " + code + " is not supported!");
     }
-    throw new UnsupportedOperationException(
-        "The code " + code + " is not supported!"
-    );
+    return result;
+  }
+
+  private static Map<String, LimitedAccess> initializeMapping() {
+    Map result = new HashMap<String, LimitedAccess>();
+    for (LimitedAccess limitedAccess : LimitedAccess.values()) {
+      result.put(limitedAccess.code, limitedAccess);
+    }
+    return result;
   }
 
   public String getCode() {
     return code;
+  }
+
+  public String getDescription() {
+    return description;
   }
 }
 
