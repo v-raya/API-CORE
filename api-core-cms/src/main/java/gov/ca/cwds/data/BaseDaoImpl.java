934b1495528f3ec7e48ab1a33257c287bd763549
David R. Smith <David.Smith@osi.ca.gov>
2017-11-15 21:17:14 -0800

    #152550170: Neutron: code coverage

--- api-core-cms/src/main/java/gov/ca/cwds/data/BaseDaoImpl.java
+++ api-core-cms/src/main/java/gov/ca/cwds/data/BaseDaoImpl.java
@@ -55,7 +55,7 @@ public abstract class BaseDaoImpl<T extends PersistentObject> extends CrudsDaoIm
     Transaction txn = session.getTransaction();
     txn = txn != null ? txn : session.beginTransaction();
 
-    if (TransactionStatus.NOT_ACTIVE.equals(txn.getStatus()) || !txn.isActive()) {
+    if (TransactionStatus.NOT_ACTIVE == txn.getStatus() || !txn.isActive()) {
       txn.begin();
     }
 
@@ -90,7 +90,7 @@ public abstract class BaseDaoImpl<T extends PersistentObject> extends CrudsDaoIm
           .setTimestamp("after", new java.sql.Timestamp(datetime.getTime()));
 
       // Iterate, process, flush.
-      final int fetchSize = 5000;
+      int fetchSize = 5000;
       query.setFetchSize(fetchSize);
       ScrollableResults results = query.scroll(ScrollMode.FORWARD_ONLY);
       ImmutableList.Builder<T> ret = new ImmutableList.Builder<>();
@@ -109,170 +109,6 @@ public abstract class BaseDaoImpl<T extends PersistentObject> extends CrudsDaoIm
       results.close();
       txn.commit();
       return ret.build();
-    } catch (HibernateException h) {
-      txn.rollback();
-      throw new DaoException(h);
-    }
-  }
-
-  /**
-   * Retrieve all records for batch processing for a single bucket. PostgreSQL queries would likely
-   * rely on the <a href="https://www.postgresql.org/docs/9.6/static/functions-window.html">NTILE
-   * analytic function</a>, whereas DB2 10.5, lacking modern analytics, would likely rely on nested
-   * or correlated queries. Note that DB2 doesn't even provide basic pseudo-columns, like ROWNUM,
-   * without enabling <a href=
-   * "http://www.ibm.com/support/knowledgecenter/SSEPGG_10.5.0/com.ibm.db2.luw.apdv.porting.doc/doc/r0052867.html">
-   * "compatibility vectors"</a> or writing a
-   * <a href="http://hoteljavaopensource.blogspot.com/2011/06/ntile-and-db2.html">user-defined
-   * function</a>.
-   * 
-   * <p>
-   * The following DB2 SQL demonstrates how to simulate the NTILE analytic without installing
-   * additional packages or non-standard functions.
-   * </p>
-   * 
-   * <pre>
-   * {@code 
-   * select z.identifier, z.bucket, z.rn, row_number() over (partition by z.bucket order by 1) as bucket_rn 
-   * from ( 
-   *    select mod(y.rn,10) + 1 as bucket, y.rn, y.identifier 
-   *    from (
-   *       select row_number() over (order by 1) as rn, x.identifier 
-   *       from ( select c.identifier from client_t c 
-   *       WHERE x.identifier >= 'B3bMRWu8NW' and x.identifier < 'DW5GzxJ30A') x 
-   *    ) y 
-   * ) z 
-   * where z.bucket = 3 FOR READ ONLY WITH UR;
-   * }
-   * </pre>
-   * 
-   * <p>
-   * Most batch bucket queries are defined Hibernate named queries in the persistence class itself.
-   * </p>
-   * 
-   * @param bucketNum current bucket for this batch
-   * @param totalBuckets total buckets for this batch run only, NOT the total for all batches
-   * @param minId minimum key value, inclusive
-   * @param maxId maximum key value, exclusive
-   * @return ordered list of referral/client document records
-   */
-  @Override
-  @SuppressWarnings("unchecked")
-  public List<T> partitionedBucketList(long bucketNum, long totalBuckets, String minId,
-      String maxId) {
-    final String namedQueryName = getEntityClass().getName() + ".findPartitionedBuckets";
-    final Session session = getSessionFactory().getCurrentSession();
-    final Transaction txn = session.beginTransaction();
-
-    try {
-      final Query query = session.getNamedQuery(namedQueryName)
-          .setInteger("bucket_num", (int) bucketNum).setInteger("total_buckets", (int) totalBuckets)
-          .setString("min_id", minId).setString("max_id", maxId).setCacheable(false)
-          .setFlushMode(FlushMode.MANUAL).setReadOnly(true).setCacheMode(CacheMode.IGNORE);
-
-      // Iterate, process, flush.
-      final int fetchSize = 5000;
-      query.setFetchSize(fetchSize);
-      ScrollableResults results = query.scroll(ScrollMode.FORWARD_ONLY);
-      ImmutableList.Builder<T> ret = new ImmutableList.Builder<>();
-      int cnt = 0;
-
-      while (results.next()) {
-        Object[] row = results.get();
-        ret.add((T) row[0]);
-
-        if (((++cnt) % fetchSize) == 0) {
-          session.flush();
-        }
-      }
-
-      session.flush();
-      results.close();
-      txn.commit();
-      return ret.build();
-    } catch (HibernateException h) {
-      txn.rollback();
-      throw new DaoException(h);
-    }
-  }
-
-  /**
-   * Retrieve all records for batch processing for a single "bucket" (an arbitrary number of
-   * records). PostgreSQL queries would likely rely on the
-   * <a href="https://www.postgresql.org/docs/9.6/static/functions-window.html">NTILE analytic
-   * function</a>, whereas DB2 10.5, lacking modern analytics, would likely rely on nested or
-   * correlated queries. Note that DB2 doesn't even provide common pseudo-columns, like ROWNUM,
-   * without enabling <a href=
-   * "http://www.ibm.com/support/knowledgecenter/SSEPGG_10.5.0/com.ibm.db2.luw.apdv.porting.doc/doc/r0052867.html">
-   * "compatibility vectors"</a> or writing a
-   * <a href="http://hoteljavaopensource.blogspot.com/2011/06/ntile-and-db2.html">user-defined
-   * function</a>.
-   * 
-   * <p>
-   * The following DB2 SQL demonstrates how to simulate the NTILE analytic without installing
-   * additional packages or non-standard functions.
-   * </p>
-   * 
-   * <pre>
-   * select z.identifier, z.bucket, z.rn, row_number() over (partition by z.bucket order by 1) as bucket_rn 
-   * from ( 
-   *    select mod(y.rn, :total_buckets) + 1 as bucket, y.rn, y.identifier 
-   *    from (
-   *       select row_number() over (order by 1) as rn, x.identifier 
-   *       from ( select c.identifier from client_t c ) x 
-   *    ) y 
-   * ) z 
-   * where z.bucket = :bucket_num fFOR READ ONLY WITH UR;
-   * </pre>
-   * 
-   * <p>
-   * Batch bucket queries are defined Hibernate named queries in the persistence class itself.
-   * </p>
-   * 
-   * <p>
-   * Since the large CMS DB2 tables are usually partitioned by primary key (column IDENTIFIER)
-   * across 16 partitions, this method takes a minimum and maximum key to enhance performance via
-   * partition pruning.
-   * </p>
-   * 
-   * @param bucketNum current bucket for this batch
-   * @param totalBuckets total buckets in batch run
-   * @return ordered list of referral/client document records
-   */
-  @Override
-  @SuppressWarnings("unchecked")
-  public List<T> bucketList(long bucketNum, long totalBuckets) {
-    final String namedQueryName = constructNamedQueryName("findAllByBucket");
-    Session session = getSessionFactory().getCurrentSession();
-    Transaction txn = session.beginTransaction();
-    try {
-      Query query = session.getNamedQuery(namedQueryName).setCacheable(false)
-          .setFlushMode(FlushMode.MANUAL).setReadOnly(true).setCacheMode(CacheMode.IGNORE)
-          .setInteger("bucket_num", (int) bucketNum)
-          .setInteger("total_buckets", (int) totalBuckets);
-
-      // Iterate, process, flush.
-      final int fetchSize = 5000;
-      query.setFetchSize(fetchSize);
-      ScrollableResults results = query.scroll(ScrollMode.FORWARD_ONLY);
-      ImmutableList.Builder<T> ret = new ImmutableList.Builder<>();
-      int cnt = 0;
-
-      while (results.next()) {
-        Object[] row = results.get();
-        ret.add((T) row[0]);
-
-        if (((++cnt) % fetchSize) == 0) {
-          LOGGER.info("recs read: {}", cnt);
-          session.flush();
-        }
-      }
-
-      session.flush();
-      results.close();
-      txn.commit();
-      return ret.build();
-
     } catch (HibernateException h) {
       txn.rollback();
       throw new DaoException(h);
--- api-core-common/src/test/java/gov/ca/cwds/rest/serializer/ErrorMessageSerializerTest.java
+++ api-core-common/src/test/java/gov/ca/cwds/rest/serializer/ErrorMessageSerializerTest.java
@@ -1,16 +1,18 @@
 package gov.ca.cwds.rest.serializer;
 
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializerProvider;
-import gov.ca.cwds.rest.api.domain.error.ErrorMessage;
-import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.util.HashSet;
 import java.util.Set;
+
 import org.junit.Before;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import gov.ca.cwds.rest.api.domain.error.ErrorMessage;
 
 public class ErrorMessageSerializerTest {
 
@@ -24,7 +26,7 @@ public class ErrorMessageSerializerTest {
   ErrorMessageSerializer writer;
 
   @Before
-  public void setup() throws IOException {
+  public void setup() throws Exception {
     jsonWriter = new StringWriter();
     jsonGenerator = new JsonFactory().createGenerator(jsonWriter);
     serializerProvider = new ObjectMapper().getSerializerProvider();
@@ -49,7 +51,7 @@ public class ErrorMessageSerializerTest {
   // assertEquals("expected json to contain both error messages",expectedJson,
   // jsonWriter.toString());
   // }
-  //
+
   // @Test
   // public void serializeSingleErrorMessage() throws IOException {
   // messages.remove(businessError);
