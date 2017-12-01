6196a34fbccdb82f0fe2ec17f084045dcfcaf211
Denys Davydov <dom8833@ya.ru>
2017-12-01 14:57:51 -0800

    FIX: Enum Entities codes are trimmed after getting from db

--- legacy-data-access/src/main/java/gov/ca/cwds/data/legacy/cms/entity/enums/BaseEntityEnumConverter.java
+++ legacy-data-access/src/main/java/gov/ca/cwds/data/legacy/cms/entity/enums/BaseEntityEnumConverter.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import javax.persistence.AttributeConverter;
 import org.apache.commons.lang3.StringUtils;
 
-public abstract class BaseEntityEnumConvertor<E extends EntityEnum<K>, K> implements
+public abstract class BaseEntityEnumConverter<E extends EntityEnum<K>, K> implements
     AttributeConverter<E, K> {
 
   static <K, E> Map<K, E> initializeMapping(EntityEnum<K>[] values) {
@@ -27,19 +27,12 @@ public abstract class BaseEntityEnumConvertor<E extends EntityEnum<K>, K> implem
 
   @Override
   public E convertToEntityAttribute(K code) {
-    if (code == null) {
-      return null;
-    }
-
-    if ((code instanceof String) && StringUtils.isBlank((String) code)) {
+    if (code == null || isEmptyString(code) || isEmptyCharacter(code)) {
         return null;
     }
 
-    if ((code instanceof Character) && code.equals(' ')) {
-        return null;
-    }
-
-    E result = getCodeMap().get(code);
+    K normalizedCode = normalizeCode(code);
+    E result = getCodeMap().get(normalizedCode);
 
     if (result == null) {
       Class<?> enumClass = Generics.getTypeParameter(getClass(), EntityEnum.class);
@@ -48,6 +41,20 @@ public abstract class BaseEntityEnumConvertor<E extends EntityEnum<K>, K> implem
           enumClassName + " enum : the code '" + code + "' is not supported!");
     }
     return result;
+  }
+
+  private boolean isEmptyString(K code) {
+    return (code instanceof String) && StringUtils.isBlank((String) code);
+  }
+
+  private boolean isEmptyCharacter(K code) {
+    return (code instanceof Character) && code.equals(' ');
+  }
+
+  private K normalizeCode(K code) {
+    return code instanceof String
+        ? (K) ((String) code).trim()
+        : code;
   }
 
   abstract Map<K, E> getCodeMap();
