0def054f91c70d5ef5d37bb11450f81a2212f75b
Alexander Serbin <alexander.serbin@engagepoint.com>
2017-12-08 07:54:43 -0800

    TPT2-38 added auto-discovery for drools module installation procedure

--- drools-engine/src/main/java/gov/ca/cwds/drools/DroolsKieContainerInstaller.java
+++ drools-engine/src/main/java/gov/ca/cwds/drools/DroolsKieContainerInstaller.java
@@ -2,24 +2,22 @@ package gov.ca.cwds.drools;
 
 import static org.drools.compiler.kproject.models.KieModuleModelImpl.KMODULE_FILE_NAME;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
-import java.util.Arrays;
 import org.apache.commons.io.IOUtils;
 import org.kie.api.KieServices;
 import org.kie.api.builder.KieBuilder;
 import org.kie.api.builder.KieFileSystem;
 import org.kie.api.builder.Message;
 import org.kie.api.runtime.KieContainer;
-import org.kie.internal.io.ResourceFactory;
 
 /**
  * @author CWDS CALS API Team
  */
 
 class DroolsKieContainerInstaller {
+
+  private static final String AUTO_DISCOVERY_FILENAME = "auto-discovery";
 
   private final KieFileSystem kfs;
   private String containerId;
@@ -48,11 +46,13 @@ class DroolsKieContainerInstaller {
     }
   }
 
-  private void addDrlFiles() {
-    URL url = getClassLoader().getResource(containerId);
-    File[] drls = new File(url.getPath())
-        .listFiles(pathname -> pathname.getAbsolutePath().endsWith("drl"));
-    Arrays.stream(drls).map(ResourceFactory::newFileResource).forEach(kfs::write);
+  private void addDrlFiles() throws IOException {
+    for (String resourceName: IOUtils.readLines(getResourceStream(AUTO_DISCOVERY_FILENAME), "UTF-8")) {
+      if (resourceName.endsWith(".drl")) {
+        kfs.write("src/main/resources/" + resourceName,
+            IOUtils.toByteArray(getResourceStream(resourceName)));
+      }
+    }
   }
 
   private ClassLoader getClassLoader() {
@@ -60,9 +60,13 @@ class DroolsKieContainerInstaller {
   }
 
   private void addKModuleXml() throws IOException {
-    InputStream inputStream = getClassLoader()
-        .getResourceAsStream(containerId + "/" + KMODULE_FILE_NAME);
+    InputStream inputStream = getResourceStream(KMODULE_FILE_NAME);
     kfs.writeKModuleXML(IOUtils.toByteArray(inputStream));
+  }
+
+  private InputStream getResourceStream(String resourceName) {
+    return getClassLoader()
+          .getResourceAsStream(containerId + "/" + resourceName);
   }
 
 }
