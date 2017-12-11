1af1a48725d761adbf34005e61e1436f1b2cbf96
dmitry.rudenko <dmitry.rudenko@engagepoint.com>
2017-12-11 14:50:26 -0800

    Placement Home: implement DocTool rule R - 03855 Max Age 25

--- legacy-data-access-services/src/test/java/gov/ca/cwds/cms/data/access/service/impl/PlacementHomeServiceImplTest.java
+++ legacy-data-access-services/src/test/java/gov/ca/cwds/cms/data/access/service/impl/PlacementHomeServiceImplTest.java
@@ -78,27 +78,4 @@ public class PlacementHomeServiceImplTest {
     PlacementHome placementHome = getPlacementHome(-1, "CT", null);
     runBusinessValidation(placementHome);
   }
-
-  @Test
-  public void testMaxAgeInvalid() throws Exception {
-    PlacementHome placementHome = new PlacementHome();
-    placementHome.setAgeToNo((short) 27);
-    try {
-      runBusinessValidation(placementHome);
-      throw new Exception("Business Validation exception must be thrown");
-    } catch (BusinessValidationException e) {
-       assert e.getValidationDetailsList().stream().anyMatch(issueDetails -> issueDetails.getCode().equals("R - 03855"));
-    }
-  }
-
-  @Test
-  public void testMaxAgeValid() throws Exception {
-    PlacementHome placementHome = new PlacementHome();
-    placementHome.setAgeToNo((short) 23);
-    try {
-      runBusinessValidation(placementHome);
-    } catch (BusinessValidationException e) {
-      assert e.getValidationDetailsList().stream().noneMatch(issueDetails -> issueDetails.getCode().equals("R - 03855"));
-    }
-  }
 }
