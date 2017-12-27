314f59c57559c519dbf672901695da77f25330d2
dmytro.palczewski <dmytro.palczewski@engagepoint.com>
2017-12-26 16:32:19 -0800

    TPT3-92: tests are refactored

--- legacy-data-access-services/src/test/java/gov/ca/cwds/cms/data/access/service/impl/BaseDocToolRulesClientTest.java
+++ legacy-data-access-services/src/test/java/gov/ca/cwds/cms/data/access/service/impl/BaseDocToolRulesClientTest.java
@@ -1,7 +1,11 @@
 package gov.ca.cwds.cms.data.access.service.impl;
 
+import static org.junit.Assert.fail;
+
 import gov.ca.cwds.cms.data.access.dto.ClientEntityAwareDTO;
 import gov.ca.cwds.data.legacy.cms.entity.Client;
+import gov.ca.cwds.drools.DroolsException;
+import gov.ca.cwds.rest.exception.BusinessValidationException;
 import org.junit.Before;
 
 public abstract class BaseDocToolRulesClientTest extends BaseDocToolRulesTest {
@@ -19,7 +23,28 @@ public abstract class BaseDocToolRulesClientTest extends BaseDocToolRulesTest {
     return "";
   }
 
-  public static ClientEntityAwareDTO clientEntityAwareDTO(Client client) {
+  protected void runBusinessValidation(Client client) throws DroolsException {
+    clientCoreService.runBusinessValidation(clientEntityAwareDTO(client), principal);
+  }
+
+  protected void checkRuleViolatedOnce(Client client, String ruleName) throws DroolsException {
+    try {
+      runBusinessValidation(client);
+      fail();
+    } catch (BusinessValidationException e) {
+      assertRuleViolatedOnce(ruleName, e);
+    }
+  }
+
+  protected void checkRuleSatisfied(Client client, String ruleName) throws DroolsException {
+    try {
+      runBusinessValidation(client);
+    } catch (BusinessValidationException e) {
+      assertRuleSatisfied(ruleName, e);
+    }
+  }
+
+  private ClientEntityAwareDTO clientEntityAwareDTO(Client client) {
     ClientEntityAwareDTO clientEntityAwareDTO = new ClientEntityAwareDTO();
     clientEntityAwareDTO.setEntity(client);
     return clientEntityAwareDTO;
--- legacy-data-access-services/src/test/java/gov/ca/cwds/cms/data/access/service/impl/R00741Test.java
+++ legacy-data-access-services/src/test/java/gov/ca/cwds/cms/data/access/service/impl/R00741Test.java
@@ -1,14 +1,9 @@
 package gov.ca.cwds.cms.data.access.service.impl;
 
-import static org.junit.Assert.fail;
-
 import gov.ca.cwds.data.legacy.cms.entity.Client;
-import gov.ca.cwds.drools.DroolsException;
-import gov.ca.cwds.rest.exception.BusinessValidationException;
 import org.junit.Test;
 
 public class R00741Test extends BaseDocToolRulesClientTest {
-
 
   @Test
   public void testLanguagesAreDifferent() throws Exception {
@@ -16,11 +11,7 @@ public class R00741Test extends BaseDocToolRulesClientTest {
     client.setPrimaryLanguageCode((short) 1);
     client.setSecondaryLanguageCode((short) 2);
 
-    try {
-      runBusinessValidation(client);
-    } catch (BusinessValidationException e) {
-      assertRuleSatisfied("R-R00741", e);
-    }
+    checkRuleSatisfied(client, "R-R00741");
   }
 
   @Test
@@ -29,11 +20,7 @@ public class R00741Test extends BaseDocToolRulesClientTest {
     client.setPrimaryLanguageCode((short) 1);
     client.setSecondaryLanguageCode((short) 0);
 
-    try {
-      runBusinessValidation(client);
-    } catch (BusinessValidationException e) {
-      assertRuleSatisfied("R-R00741", e);
-    }
+    checkRuleSatisfied(client, "R-R00741");
   }
 
   @Test
@@ -42,12 +29,7 @@ public class R00741Test extends BaseDocToolRulesClientTest {
     client.setPrimaryLanguageCode((short) 1);
     client.setSecondaryLanguageCode((short) 1);
 
-    try {
-      runBusinessValidation(client);
-      fail();
-    } catch (BusinessValidationException e) {
-      assertRuleViolatedOnce("R-00741", e);
-    }
+    checkRuleViolatedOnce(client, "R-00741");
   }
 
   @Test
@@ -56,14 +38,6 @@ public class R00741Test extends BaseDocToolRulesClientTest {
     client.setPrimaryLanguageCode((short) 0);
     client.setSecondaryLanguageCode((short) 0);
 
-    try {
-      runBusinessValidation(client);
-    } catch (BusinessValidationException e) {
-      assertRuleSatisfied("R-R00741", e);
-    }
-  }
-
-  private void runBusinessValidation(Client client) throws DroolsException {
-    clientCoreService.runBusinessValidation(clientEntityAwareDTO(client), principal);
+    checkRuleSatisfied(client, "R-R00741");
   }
 }
