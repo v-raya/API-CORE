72e2a0725c48999b085452b1724e299ee1d80d1e
sorych <dmytro.sorych@osi.ca.gov>
2018-01-05 10:07:32 -0800

    NEW TPT3-104 R - 00742 rule for Client Update

---
+++ legacy-data-access-services/src/test/java/gov/ca/cwds/cms/data/access/service/impl/R00742Test.java
@@ -0,0 +1,100 @@
+package gov.ca.cwds.cms.data.access.service.impl;
+
+import gov.ca.cwds.data.legacy.cms.entity.Client;
+import gov.ca.cwds.data.legacy.cms.entity.ClientScpEthnicity;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class R00742Test extends BaseDocToolRulesClientTest {
+
+    private static final String RULE_NAME = "R-00742";
+
+    @Test
+    public void testValidListofEthnicities() throws Exception {
+        Client client = new Client();
+        client.setIdentifier("1234567890");
+        client.setPrimaryEthnicityCode((short) 1);
+
+        clientEntityAwareDTO.setEntity(client);
+
+        List<ClientScpEthnicity> clientScpEthnicityList = createListOfDifferentClientScpEthnicities();
+        clientEntityAwareDTO.getClientScpEthnicities().addAll(clientScpEthnicityList);
+
+        checkRuleSatisfied(RULE_NAME);
+    }
+
+    @Test
+    public void testOtherEthnicityPresentWhenNoPrimary() throws Exception {
+        Client client = new Client();
+        client.setIdentifier("1234567890");
+        client.setPrimaryEthnicityCode((short) 0);
+
+        clientEntityAwareDTO.setEntity(client);
+
+        List<ClientScpEthnicity> clientScpEthnicityList = createListOfDifferentClientScpEthnicities();
+        clientEntityAwareDTO.getClientScpEthnicities().addAll(clientScpEthnicityList);
+
+        checkRuleViolatedOnce(RULE_NAME);
+    }
+
+    @Test
+    public void testEmptyListOfOtherEthnicitiesWhenPrimaryPresent() throws Exception {
+        Client client = new Client();
+        client.setIdentifier("1234567890");
+        client.setPrimaryEthnicityCode((short) 1);
+
+        clientEntityAwareDTO.setEntity(client);
+
+        checkRuleSatisfied(RULE_NAME);
+    }
+
+    @Test
+    public void testEmptyListOfOtherEthnicitiesWhenNoPrimary() throws Exception {
+        Client client = new Client();
+        client.setIdentifier("1234567890");
+        client.setPrimaryEthnicityCode((short) 0);
+
+        clientEntityAwareDTO.setEntity(client);
+
+        checkRuleSatisfied(RULE_NAME);
+    }
+
+    @Test
+    public void testNoPrimaryValidListOfOther() throws Exception {
+        Client client = new Client();
+        client.setIdentifier("1234567890");
+        client.setPrimaryEthnicityCode((short) 0);
+
+        clientEntityAwareDTO.setEntity(client);
+
+        clientEntityAwareDTO.getClientScpEthnicities().add(createClientScpEthnicity("C", "1234567890", (short) 0));
+        clientEntityAwareDTO.getClientScpEthnicities().add(createClientScpEthnicity("C", "1234567890", (short) 0));
+
+        checkRuleSatisfied(RULE_NAME);
+    }
+
+
+    private static List<ClientScpEthnicity> createListOfDifferentClientScpEthnicities() {
+        List<ClientScpEthnicity> clientScpEthnicityList = new ArrayList<>();
+        ClientScpEthnicity clientScpEthnicity1 = createClientScpEthnicity("C", "1234567890", (short) 3162);
+        ClientScpEthnicity clientScpEthnicity2 = createClientScpEthnicity("C", "1234567890", (short) 3163);
+        ClientScpEthnicity clientScpEthnicity3 = createClientScpEthnicity("C", "1234567890", (short) 0);
+
+        clientScpEthnicityList.addAll(Arrays.asList(clientScpEthnicity1, clientScpEthnicity2, clientScpEthnicity3));
+        return clientScpEthnicityList;
+    }
+
+
+    private static ClientScpEthnicity createClientScpEthnicity(String establishCode, String establishId, Short ethnicityCode) {
+        ClientScpEthnicity clientScpEthnicity = new ClientScpEthnicity();
+        clientScpEthnicity.setEstblshCd(establishCode);
+        clientScpEthnicity.setEstblshId(establishId);
+        clientScpEthnicity.setEthnctyc(ethnicityCode);
+        return clientScpEthnicity;
+    }
+
+
+}
