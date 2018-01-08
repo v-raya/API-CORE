e027c4986253a1a0f01e7f979952d253b8d0a3fa
sorych <dmytro.sorych@osi.ca.gov>
2018-01-08 15:24:02 -0800

    ClientScpEthnicityDao created and ClientAwareDTO fullfilment implemented

--- legacy-data-access-services/src/main/java/gov/ca/cwds/cms/data/access/service/impl/ClientCoreServiceImpl.java
+++ legacy-data-access-services/src/main/java/gov/ca/cwds/cms/data/access/service/impl/ClientCoreServiceImpl.java
@@ -6,59 +6,77 @@ import gov.ca.cwds.cms.data.access.service.DataAccessServicesException;
 import gov.ca.cwds.cms.data.access.service.ClientCoreService;
 import gov.ca.cwds.cms.data.access.service.rules.ClientDroolsConfiguration;
 import gov.ca.cwds.data.legacy.cms.dao.ClientDao;
+import gov.ca.cwds.data.legacy.cms.dao.ClientScpEthnicityDao;
 import gov.ca.cwds.data.legacy.cms.entity.Client;
+import gov.ca.cwds.data.legacy.cms.entity.ClientScpEthnicity;
 import gov.ca.cwds.drools.DroolsException;
 import gov.ca.cwds.drools.DroolsService;
 import gov.ca.cwds.rest.exception.BusinessValidationException;
 import gov.ca.cwds.rest.exception.IssueDetails;
 import gov.ca.cwds.security.realm.PerryAccount;
 import gov.ca.cwds.security.utils.PrincipalUtils;
+
+import java.util.List;
 import java.util.Set;
 
 public class ClientCoreServiceImpl implements ClientCoreService {
 
-  @Inject private DroolsService droolsService;
+    @Inject
+    private DroolsService droolsService;
 
-  @Inject private ClientDao clientDao;
+    @Inject
+    private ClientDao clientDao;
 
-  @Override
-  public Client create(ClientEntityAwareDTO entityAwareDTO) throws DataAccessServicesException {
-    throw new UnsupportedOperationException();
-  }
+    @Inject
+    private ClientScpEthnicityDao clientScpEthnicityDao;
 
-  @Override
-  public Client update(ClientEntityAwareDTO clientEntityAwareDTO)
-      throws DataAccessServicesException {
-    try {
-      runBusinessValidation(clientEntityAwareDTO, PrincipalUtils.getPrincipal());
-      updateClient(clientEntityAwareDTO);
-      return clientEntityAwareDTO.getEntity();
-    } catch (DroolsException e) {
-      throw new DataAccessServicesException(e);
+    @Override
+    public Client create(ClientEntityAwareDTO entityAwareDTO) throws DataAccessServicesException {
+        throw new UnsupportedOperationException();
     }
-  }
 
-  @Override
-  public void runBusinessValidation(ClientEntityAwareDTO clientEntityAwareDTO, PerryAccount principal)
-      throws DroolsException {
-    Set<IssueDetails> detailsList =
-        droolsService.performBusinessRules(
-            ClientDroolsConfiguration.INSTANCE, clientEntityAwareDTO, principal);
-    if (!detailsList.isEmpty()) {
-      throw new BusinessValidationException("Can't create Client", detailsList);
+    @Override
+    public Client update(ClientEntityAwareDTO clientEntityAwareDTO)
+            throws DataAccessServicesException {
+        try {
+            prepareEntityForValidation(clientEntityAwareDTO);
+            runBusinessValidation(clientEntityAwareDTO, PrincipalUtils.getPrincipal());
+            updateClient(clientEntityAwareDTO);
+            return clientEntityAwareDTO.getEntity();
+        } catch (DroolsException e) {
+            throw new DataAccessServicesException(e);
+        }
     }
-  }
 
-  private void updateClient(ClientEntityAwareDTO clientEntityAwareDTO) {
-    final Client client = clientEntityAwareDTO.getEntity();
-    clientDao.update(client);
-  }
+    private void prepareEntityForValidation(ClientEntityAwareDTO clientEntityAwareDTO) {
+        if (clientEntityAwareDTO.getEntity() != null) {
+            List<ClientScpEthnicity> clientScpEthnicityList = clientScpEthnicityDao.findEthnicitiesByClient(clientEntityAwareDTO.getEntity().getIdentifier());
+            clientEntityAwareDTO.getClientScpEthnicities().addAll(clientScpEthnicityList);
+        }
 
-  public void setDroolsService(DroolsService droolsService) {
-    this.droolsService = droolsService;
-  }
+    }
 
-  public void setClientDao(ClientDao clientDao) {
-    this.clientDao = clientDao;
-  }
+    @Override
+    public void runBusinessValidation(ClientEntityAwareDTO clientEntityAwareDTO, PerryAccount principal)
+            throws DroolsException {
+        Set<IssueDetails> detailsList =
+                droolsService.performBusinessRules(
+                        ClientDroolsConfiguration.INSTANCE, clientEntityAwareDTO, principal);
+        if (!detailsList.isEmpty()) {
+            throw new BusinessValidationException("Can't create Client", detailsList);
+        }
+    }
+
+    private void updateClient(ClientEntityAwareDTO clientEntityAwareDTO) {
+        final Client client = clientEntityAwareDTO.getEntity();
+        clientDao.update(client);
+    }
+
+    public void setDroolsService(DroolsService droolsService) {
+        this.droolsService = droolsService;
+    }
+
+    public void setClientDao(ClientDao clientDao) {
+        this.clientDao = clientDao;
+    }
 }
