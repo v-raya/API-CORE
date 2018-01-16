d03034bcf10fc7c13a40ad67753c26a3a59f6859
David R. Smith <David.Smith@osi.ca.gov>
2018-01-16 07:46:46 -0800

    People summary rocket

--- legacy-data-access/src/main/java/gov/ca/cwds/data/legacy/cms/dao/CaseDao.java
+++ legacy-data-access/src/main/java/gov/ca/cwds/data/legacy/cms/dao/CaseDao.java
@@ -1,7 +1,14 @@
 package gov.ca.cwds.data.legacy.cms.dao;
 
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+
 import com.google.common.collect.ImmutableList;
 import com.google.inject.Inject;
+
 import gov.ca.cwds.data.CrudsDaoImpl;
 import gov.ca.cwds.data.legacy.cms.entity.Case;
 import gov.ca.cwds.data.legacy.cms.entity.CaseAssignment;
@@ -10,10 +17,6 @@ import gov.ca.cwds.data.legacy.cms.entity.CaseLoadWeighting;
 import gov.ca.cwds.data.legacy.cms.entity.StaffPerson;
 import gov.ca.cwds.inject.CmsSessionFactory;
 import gov.ca.cwds.util.Require;
-import java.time.LocalDate;
-import java.util.Collection;
-import java.util.List;
-import org.hibernate.SessionFactory;
 
 /**
  * DAO for {@link Case}.
@@ -33,17 +36,17 @@ public class CaseDao extends CrudsDaoImpl<Case> {
   }
 
   /**
-   * Returns cases which have relation to {@link StaffPerson}
-   * via {@link CaseAssignment}, {@link CaseLoad} and {@link CaseLoadWeighting}
-   * entities. Also the {@link CaseAssignment} is active, which means {@param activeDate} is between
-   * startDate and endDate of the assignment.
+   * Returns cases which have relation to {@link StaffPerson} via {@link CaseAssignment},
+   * {@link CaseLoad} and {@link CaseLoadWeighting} entities. Also the {@link CaseAssignment} is
+   * active, which means param activeDate is between startDate and endDate of the assignment.
    *
    * @param staffId - Identifier of Staff Person who can work on the returned cases.
-   * @param activeDate - The returned cases assignment will be active on this date.
-   * As usual this param is a current date.
+   * @param activeDate - The returned cases assignment will be active on this date. As usual this
+   *        param is a current date.
    * @return Cases with active assignments that can be managed by requested staff person
    */
-  public Collection<Case> findByStaffIdAndActiveDate(final String staffId, final LocalDate activeDate) {
+  public Collection<Case> findByStaffIdAndActiveDate(final String staffId,
+      final LocalDate activeDate) {
     Require.requireNotNullAndNotEmpty(staffId);
 
     final List<Case> cases = currentSession()
@@ -57,19 +60,17 @@ public class CaseDao extends CrudsDaoImpl<Case> {
 
   public List<Case> findActiveByClient(String clientId) {
 
-    List<Case> cases = currentSession()
-            .createNamedQuery(Case.NQ_FIND_ACTIVE_BY_CLIENT_ID, Case.class)
-            .setParameter(Case.NQ_PARAM_CLIENT_ID, clientId)
-            .list();
+    List<Case> cases =
+        currentSession().createNamedQuery(Case.NQ_FIND_ACTIVE_BY_CLIENT_ID, Case.class)
+            .setParameter(Case.NQ_PARAM_CLIENT_ID, clientId).list();
 
     return ImmutableList.<Case>builder().addAll(cases).build();
   }
 
   public List<Case> findClosedByClient(String clientId) {
-    List<Case> cases = currentSession()
-            .createNamedQuery(Case.NQ_FIND_CLOSED_BY_CLIENT_ID, Case.class)
-            .setParameter(Case.NQ_PARAM_CLIENT_ID, clientId)
-            .list();
+    List<Case> cases =
+        currentSession().createNamedQuery(Case.NQ_FIND_CLOSED_BY_CLIENT_ID, Case.class)
+            .setParameter(Case.NQ_PARAM_CLIENT_ID, clientId).list();
 
     return ImmutableList.<Case>builder().addAll(cases).build();
   }
