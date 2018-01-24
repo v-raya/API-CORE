fe306a850f1c86cfbaaefe03d2fbc5f88ae75361
Denys Davydov <dom8833@ya.ru>
2018-01-23 16:35:59 -0800

    NEW: TPT3-144. Safety Alerts dao implementation

---
+++ legacy-data-access/src/main/java/gov/ca/cwds/data/legacy/cms/entity/syscodes/SafetyAlertActivationReasonType.java
@@ -0,0 +1,22 @@
+package gov.ca.cwds.data.legacy.cms.entity.syscodes;
+
+import javax.persistence.Cacheable;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import org.hibernate.annotations.NamedQuery;
+
+/**
+ * @author CWDS TPT-3 Team
+ */
+
+@Entity
+@Cacheable
+@DiscriminatorValue(value = "ACTV_RNC")
+@NamedQuery(
+    name = SafetyAlertActivationReasonType.NQ_ALL,
+    query = "FROM SafetyAlertActivationReasonType"
+)
+public class SafetyAlertActivationReasonType extends SystemCodeTable {
+  public static final String NQ_ALL = "SafetyAlertActivationReasonType.all";
+  private static final long serialVersionUID = -4032664807617057298L;
+}
