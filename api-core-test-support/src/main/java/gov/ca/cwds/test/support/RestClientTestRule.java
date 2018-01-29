71a0abdacf0d5ccc42d06fc093f36d5fce0b47a2
Alexander Serbin <alserbin@gmail.com>
2018-01-29 13:15:56 -0800

    CALS-5107 added security capabilities to test framework

--- api-core-test-support/src/main/java/gov/ca/cwds/test/support/RestClientTestRule.java
+++ api-core-test-support/src/main/java/gov/ca/cwds/test/support/RestClientTestRule.java
@@ -1,7 +1,10 @@
 package gov.ca.cwds.test.support;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import gov.ca.cwds.security.jwt.JwtConfiguration;
+import gov.ca.cwds.security.jwt.JwtService;
 import io.dropwizard.Configuration;
 import io.dropwizard.testing.junit.DropwizardAppRule;
 import org.apache.commons.lang3.StringUtils;
@@ -13,28 +16,104 @@ import org.junit.runners.model.Statement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.WebTarget;
+import java.io.File;
+import java.io.IOException;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+import java.util.Properties;
 
-/**
- * @author CWDS CALS API Team
- */
+import static io.dropwizard.testing.FixtureHelpers.fixture;
+
+
 public class RestClientTestRule<T extends Configuration> implements TestRule {
+
+  private final static String AUTHORIZED_ACCOUNT_FIXTURE = "default-perry-account.json";
+  private final static String DEFAULT_IDENTITY_JSON = fixture(AUTHORIZED_ACCOUNT_FIXTURE);
 
   private static final Logger LOG = LoggerFactory.getLogger(RestClientTestRule.class);
 
   private final DropwizardAppRule<T> dropWizardApplication;
 
+  private static final TrustManager[] TRUST_ALL_CERTS = new TrustManager[]{new X509TrustManager() {
+    public X509Certificate[] getAcceptedIssuers() {
+      return null;
+    }
+    public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+
+    public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+  }};
+
   private Client client;
   private ObjectMapper mapper;
+  private JwtConfiguration jwtConfiguration;
+  private String token;
 
-  public RestClientTestRule(DropwizardAppRule<T> dropWizardApplication) {
+  public RestClientTestRule(DropwizardAppRule<T> dropWizardApplication)  {
     this.dropWizardApplication = dropWizardApplication;
+    this.token = getDefaultToken();
   }
 
-  public WebTarget target(String pathInfo) {
+  private String getDefaultToken() {
+    try {
+      return generateToken(DEFAULT_IDENTITY_JSON);
+    } catch (Exception e) {
+      LOG.warn("Cannot generate token");
+      return null;
+    }
+  }
+
+  public RestClientTestRule withSecurityToken(String identityJsonFilePath) throws IOException {
+    this.token = generateToken(fixture(identityJsonFilePath));
+    return this;
+  }
+
+  private String generateToken(String identity) throws IOException {
+    JwtConfiguration configuration = getJwtConfiguration();
+    JwtService jwtService = new JwtService(configuration);
+    return jwtService.generate("id", "subject", identity);
+  }
+
+  private JwtConfiguration getJwtConfiguration() throws IOException {
+    if (jwtConfiguration != null) {
+      return jwtConfiguration;
+    }
+
+    Properties properties = new Properties();
+    properties.load(getClass().getClassLoader().getResourceAsStream("test-shiro.ini"));
+
+    jwtConfiguration = new JwtConfiguration();
+    //JWT
+    jwtConfiguration.setTimeout(30);
+    jwtConfiguration.setIssuer(properties.getProperty("perryRealm.tokenIssuer"));
+    jwtConfiguration.setKeyStore(new JwtConfiguration.KeyStoreConfiguration());
+    //KeyStore
+    jwtConfiguration.getKeyStore()
+            .setPath(new File(properties.getProperty("perryRealm.keyStorePath")).getPath());
+    jwtConfiguration.getKeyStore().setPassword(properties.getProperty("perryRealm.keyStorePassword"));
+    //Sign/Validate Key
+    jwtConfiguration.getKeyStore().setAlias(properties.getProperty("perryRealm.keyStoreAlias"));
+    jwtConfiguration.getKeyStore()
+            .setKeyPassword(properties.getProperty("perryRealm.keyStoreKeyPassword"));
+    //Enc Key
+    jwtConfiguration
+            .setEncryptionEnabled(Boolean.valueOf(properties.getProperty("perryRealm.useEncryption")));
+    jwtConfiguration.getKeyStore()
+            .setEncKeyPassword(properties.getProperty("perryRealm.encKeyPassword"));
+    jwtConfiguration.getKeyStore().setEncAlias(properties.getProperty("perryRealm.encKeyAlias"));
+    jwtConfiguration.setEncryptionMethod(properties.getProperty("perryRealm.encryptionMethod"));
+    return jwtConfiguration;
+  }
+
+
+  public WebTarget target(String pathInfo) throws IOException {
     String restUrl = getUriString() + pathInfo;
-    return client.target(restUrl);
+    WebTarget target = client.target(restUrl).queryParam("token", token);
+    token = getDefaultToken();
+    return target;
   }
 
   private String composeUriString() {
@@ -64,18 +143,22 @@ public class RestClientTestRule<T extends Configuration> implements TestRule {
       public void evaluate() throws Throwable {
 
         JerseyClientBuilder clientBuilder = new JerseyClientBuilder()
-            .property(ClientProperties.CONNECT_TIMEOUT, 5000)
-            .property(ClientProperties.READ_TIMEOUT, 60000)
-            .hostnameVerifier((hostName, sslSession) -> {
-              // Just ignore host verification for test purposes
-              return true;
-            });
+                .property(ClientProperties.CONNECT_TIMEOUT, 5000)
+                .property(ClientProperties.READ_TIMEOUT, 20000)
+                .hostnameVerifier((hostName, sslSession) -> {
+                  // Just ignore host verification for test purposes
+                  return true;
+                });
 
         client = clientBuilder.build();
-        mapper = dropWizardApplication.getObjectMapper();
         client.register(new JacksonJsonProvider(mapper));
+        client.getSslContext().init(null, TRUST_ALL_CERTS, new SecureRandom());
+        mapper = dropWizardApplication.getObjectMapper();
+        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
         statement.evaluate();
       }
     };
   }
+
 }
+
--- api-core-test-support/src/test/java/gov/ca/cwds/test/support/TestResource.java
+++ api-core-test-support/src/test/java/gov/ca/cwds/test/support/TestResource.java
@@ -1,9 +1,13 @@
 package gov.ca.cwds.test.support;
 
+import gov.ca.cwds.security.utils.PrincipalUtils;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author CWDS CALS API Team
@@ -13,8 +17,17 @@ import javax.ws.rs.core.MediaType;
 public class TestResource {
 
   @GET
-  public String get() {
+  public String checkDefaultPrincipal() {
+    assertEquals("000", PrincipalUtils.getPrincipal().getStaffId());
     return "Ok";
   }
+
+  @GET
+  @Path("/custom")
+  public String checkCustomPrincipal() {
+    assertEquals("Custom", PrincipalUtils.getPrincipal().getStaffId());
+    return "Ok";
+  }
+
 
 }
