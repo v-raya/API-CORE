0473caeea69c2342a42c1c0644f48e6f2b9adaeb
Dmytro Palczewski <dmytro.palczewski@HHSTechGroup.com>
2018-01-31 14:49:27 -0800

    TPT3-159: activeHealthInterventionPlans moved to ChildClientCoreServiceImpl

--- legacy-data-access-services/src/main/java/gov/ca/cwds/cms/data/access/service/impl/ChildClientCoreServiceImpl.java
+++ legacy-data-access-services/src/main/java/gov/ca/cwds/cms/data/access/service/impl/ChildClientCoreServiceImpl.java
@@ -1,13 +1,22 @@
 package gov.ca.cwds.cms.data.access.service.impl;
 
+import com.google.inject.Inject;
 import gov.ca.cwds.cms.data.access.dto.ChildClientEntityAwareDTO;
 import gov.ca.cwds.cms.data.access.service.ChildClientCoreService;
+import gov.ca.cwds.data.legacy.cms.dao.HealthInterventionPlanDao;
 import gov.ca.cwds.data.legacy.cms.entity.ChildClient;
 import gov.ca.cwds.data.legacy.cms.entity.FCEligibility;
+import gov.ca.cwds.data.legacy.cms.entity.HealthInterventionPlan;
 import java.util.List;
 
-/** @author CWDS TPT-3 Team */
-public class ChildClientCoreServiceImpl extends ClientCoreServiceBase<ChildClientEntityAwareDTO> implements ChildClientCoreService {
+/**
+ * @author CWDS TPT-3 Team
+ */
+public class ChildClientCoreServiceImpl extends
+    ClientCoreServiceBase<ChildClientEntityAwareDTO> implements ChildClientCoreService {
+
+  @Inject
+  private HealthInterventionPlanDao healthInterventionPlanDao;
 
   protected void enrichClientEntityAwareDto(ChildClientEntityAwareDTO clientEntityAwareDTO) {
     ChildClient childClient = (ChildClient) clientEntityAwareDTO.getEntity();
@@ -16,5 +25,14 @@ public class ChildClientCoreServiceImpl extends ClientCoreServiceBase<ChildClien
           getFcEligibilityDao().findByChildClientId(childClient.getIdentifier());
       clientEntityAwareDTO.setFcEligibilities(fcEligibilities);
     }
+
+    List<HealthInterventionPlan> activeHealthInterventionPlans =
+        healthInterventionPlanDao.getActiveHealthInterventionPlans(childClient.getIdentifier());
+    clientEntityAwareDTO.setActiveHealthInterventionPlans(activeHealthInterventionPlans);
+  }
+
+  public void setHealthInterventionPlanDao(
+      HealthInterventionPlanDao healthInterventionPlanDao) {
+    this.healthInterventionPlanDao = healthInterventionPlanDao;
   }
 }
