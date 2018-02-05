f6f8722d424b331721f5104b0a92b059d59f8449
Denys Davydov <dom8833@ya.ru>
2018-02-05 13:34:24 -0800

    FIX: Fixed SafetyAlertDao unit test

--- legacy-data-access/src/test/java/gov/ca/cwds/data/legacy/cms/dao/SafetyAlertDaoTest.java
+++ legacy-data-access/src/test/java/gov/ca/cwds/data/legacy/cms/dao/SafetyAlertDaoTest.java
@@ -9,7 +9,6 @@ import gov.ca.cwds.data.legacy.cms.persistence.BaseCwsCmsInMemoryPersistenceTest
 import java.time.LocalDate;
 import java.util.Collection;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -34,7 +33,6 @@ public class SafetyAlertDaoTest extends BaseCwsCmsInMemoryPersistenceTest {
   }
 
   @Test
-  @Ignore // TODO(dd): Fix it. Works as single test, doesn't work in the full suite
   public void findByClientId_success_whenSingleRecord() {
     executeInTransaction(sessionFactory, (sessionFactory) -> {
       // when
