8c7b670d07204acf724d22e1dc496cefd0ffcc8d
Mykhailo <m.zemlyanoi@gmail.com>
2018-02-06 11:12:37 -0800

    PT3-162 fix comments

--- legacy-data-access-services/src/main/java/gov/ca/cwds/cms/data/access/dto/ChildClientEntityAwareDTO.java
+++ legacy-data-access-services/src/main/java/gov/ca/cwds/cms/data/access/dto/ChildClientEntityAwareDTO.java
@@ -9,14 +9,13 @@ import gov.ca.cwds.data.legacy.cms.entity.MedicalEligibilityApplication;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * @author CWDS TPT-3 Team
- **/
+/** @author CWDS TPT-3 Team */
 public class ChildClientEntityAwareDTO extends ClientEntityAwareDTO {
   private final List<FCEligibility> fcEligibilities = new ArrayList<>();
   private final List<HealthInterventionPlan> activeHealthInterventionPlans = new ArrayList<>();
   private final List<ParentalRightsTermination> parentalRightsTerminations = new ArrayList<>();
-  private final List<MedicalEligibilityApplication> medicalEligibilityApplications = new ArrayList<>();
+  private final List<MedicalEligibilityApplication> medicalEligibilityApplications =
+      new ArrayList<>();
   private final List<CsecHistory> csecHistories = new ArrayList<>();
 
   public List<MedicalEligibilityApplication> getMedicalEligibilityApplications() {
