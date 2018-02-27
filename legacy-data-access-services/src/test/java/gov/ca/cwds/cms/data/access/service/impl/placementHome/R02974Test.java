27e0dd0d589e9923e44b27092cf0baf5f1ed1353
DmitriyRudenko <Dmitry.Rudenko@osi.ca.gov>
2018-02-27 14:57:58 -0800

    R - 02974 bug fix

--- legacy-data-access-services/src/test/java/gov/ca/cwds/cms/data/access/service/impl/placementHome/R02974Test.java
+++ legacy-data-access-services/src/test/java/gov/ca/cwds/cms/data/access/service/impl/placementHome/R02974Test.java
@@ -16,14 +16,12 @@ public class R02974Test extends BaseDocToolRulesPlacementHomeTest {
 
     @Test
     public void testAbsentSCPViolation() {
-        placementHomeEntityAwareDTO.getEntity().setPrimarySubstituteCareProvider(null);
         placementHomeEntityAwareDTO.getEntity().setFacilityType((short) 0);
         check("R-02974");
     }
 
     @Test
     public void testAbsentSCPForFacilityType6716() {
-        placementHomeEntityAwareDTO.getEntity().setPrimarySubstituteCareProvider(null);
         placementHomeEntityAwareDTO.getEntity().setFacilityType((short) 6716);
         assertValid("R-02974");
     }
@@ -37,7 +35,6 @@ public class R02974Test extends BaseDocToolRulesPlacementHomeTest {
 
     @Test
     public void testAbsentSCPForFacilityType6915AndTrnhsgIndIsN() {
-        placementHomeEntityAwareDTO.getEntity().setPrimarySubstituteCareProvider(null);
         placementHomeEntityAwareDTO.getEntity().setFacilityType((short) 6915);
         placementHomeEntityAwareDTO.getEntity().setTrnhsgInd("N");
         assertValid("R-02974");
