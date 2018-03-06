c3273a1ff8055b9cb2d4d6ad43435aeafb1cd8c0
sorych <dmytro.sorych@osi.ca.gov>
2018-03-05 18:44:40 -0800

    Client related entities authorization moved to api core

---
+++ legacy-data-access-services/src/main/java/gov/ca/cwds/cms/data/access/service/ClientRelationshipService.java
@@ -0,0 +1,31 @@
+package gov.ca.cwds.cms.data.access.service;
+
+import gov.ca.cwds.data.legacy.cms.dao.ClientRelationshipDao;
+import gov.ca.cwds.data.legacy.cms.entity.ClientRelationship;
+import gov.ca.cwds.security.annotations.Authorize;
+
+import javax.inject.Inject;
+import java.time.LocalDate;
+import java.util.List;
+
+import static gov.ca.cwds.cms.data.access.Constants.Authorize.CLIENT_READ_CLIENT_ID;
+
+public class ClientRelationshipService {
+
+  private final ClientRelationshipDao clientRelationshipDao;
+
+  @Inject
+  public ClientRelationshipService(final ClientRelationshipDao clientRelationshipDao) {
+    this.clientRelationshipDao = clientRelationshipDao;
+  }
+
+  public List<ClientRelationship> findRelationshipsByLeftSide(
+      @Authorize(CLIENT_READ_CLIENT_ID) final String clientId) {
+    return clientRelationshipDao.findRelationshipsByLeftSide(clientId, LocalDate.now());
+  }
+
+  public List<ClientRelationship> findRelationshipsByRightSide(
+      @Authorize(CLIENT_READ_CLIENT_ID) final String clientId) {
+    return clientRelationshipDao.findRelationshipsByRightSide(clientId, LocalDate.now());
+  }
+}
