e16ac118e42ad67b822582450a91da8aec0a8700
Dmytro Palczewski <dmytro.palczewski@HHSTechGroup.com>
2018-03-12 14:30:29 -0700

    CASE-1398: PlacementEpisodeDao is created

---
+++ legacy-data-access/src/main/java/gov/ca/cwds/data/legacy/cms/dao/PlacementEpisodeDao.java
@@ -0,0 +1,33 @@
+package gov.ca.cwds.data.legacy.cms.dao;
+
+import static gov.ca.cwds.data.legacy.cms.entity.PlacementEpisode.FIND_BY_CLIENT_ID;
+import static gov.ca.cwds.data.legacy.cms.entity.PlacementEpisode.PARAM_CLIENT_ID;
+
+import com.google.common.collect.ImmutableList;
+import com.google.inject.Inject;
+import gov.ca.cwds.data.BaseDaoImpl;
+import gov.ca.cwds.data.legacy.cms.entity.PlacementEpisode;
+import gov.ca.cwds.inject.CmsSessionFactory;
+import gov.ca.cwds.util.Require;
+import java.util.List;
+import org.hibernate.SessionFactory;
+
+public class PlacementEpisodeDao extends BaseDaoImpl<PlacementEpisode> {
+
+    @Inject
+    public PlacementEpisodeDao(@CmsSessionFactory SessionFactory sessionFactory) {
+        super(sessionFactory);
+    }
+
+    public List<PlacementEpisode> findByClientId(String clientId) {
+
+        Require.requireNotNullAndNotEmpty(clientId);
+
+        final List<PlacementEpisode> details = currentSession()
+            .createNamedQuery(FIND_BY_CLIENT_ID, PlacementEpisode.class)
+            .setParameter(PARAM_CLIENT_ID, clientId)
+            .list();
+
+        return ImmutableList.copyOf(details);
+    }
+}
