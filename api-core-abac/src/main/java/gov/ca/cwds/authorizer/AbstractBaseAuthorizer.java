4128f3d7c14e44c8f6c5937ed345de5e021b4a6a
jim lebeau <James.LeBeau@osi.ca.gov>
2018-03-13 09:19:08 -0700

    sonarqube updates

--- api-core-abac/src/main/java/gov/ca/cwds/authorizer/AbstractBaseAuthorizer.java
+++ api-core-abac/src/main/java/gov/ca/cwds/authorizer/AbstractBaseAuthorizer.java
@@ -1,24 +1,23 @@
 package gov.ca.cwds.authorizer;
 
 import static gov.ca.cwds.authorizer.util.StaffPrivilegeUtil.toStaffPersonPrivilegeTypes;
-
-import gov.ca.cwds.authorizer.drools.DroolsAuthorizationService;
-import gov.ca.cwds.authorizer.drools.configuration.DroolsAuthorizer;
-import gov.ca.cwds.drools.DroolsException;
-import gov.ca.cwds.security.authorizer.BaseAuthorizer;
-import gov.ca.cwds.security.realm.PerryAccount;
-import gov.ca.cwds.security.realm.PerrySubject;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import org.apache.shiro.authz.AuthorizationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import gov.ca.cwds.authorizer.drools.DroolsAuthorizationService;
+import gov.ca.cwds.authorizer.drools.configuration.DroolsAuthorizer;
+import gov.ca.cwds.drools.DroolsException;
+import gov.ca.cwds.security.authorizer.BaseAuthorizer;
+import gov.ca.cwds.security.realm.PerryAccount;
+import gov.ca.cwds.security.realm.PerrySubject;
 
 /**
  * @author CWDS TPT-2 Team
  */
-public abstract class AbstractBaseAuthorizer<TYPE, ID> extends BaseAuthorizer<TYPE, ID> {
+public abstract class AbstractBaseAuthorizer<T, ID> extends BaseAuthorizer<T, ID> {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(AbstractBaseAuthorizer.class);
 
@@ -31,7 +30,7 @@ public abstract class AbstractBaseAuthorizer<TYPE, ID> extends BaseAuthorizer<TY
   private void logAuthorization(
       final PerryAccount perryAccount,
       final Set<StaffPrivilegeType> staffPrivilegeTypes,
-      final TYPE instance,
+      final T instance,
       final boolean authorizationResult) {
     LOGGER.info(
         "StaffPerson [{}] with staffPrivilegeTypes = {} is creating object [{}]. "
@@ -44,7 +43,7 @@ public abstract class AbstractBaseAuthorizer<TYPE, ID> extends BaseAuthorizer<TY
   }
 
   protected boolean authorizeInstanceOperation(
-      final TYPE instance,
+      final T instance,
       final DroolsAuthorizer droolsConfiguration,
       List<Object> authorizationFacts) {
     try {
@@ -68,7 +67,7 @@ public abstract class AbstractBaseAuthorizer<TYPE, ID> extends BaseAuthorizer<TY
   }
 
   @Override
-  abstract protected boolean checkInstance(TYPE instance);
+  protected abstract boolean checkInstance(T instance);
 
   void setDroolsAuthorizationService(DroolsAuthorizationService droolsAuthorizationService) {
     this.droolsAuthorizationService = droolsAuthorizationService;
