4ce59294d54effaeb54c62dcdfc728d8631ca8c3
DmitriyRudenko <Dmitry.Rudenko@osi.ca.gov>
2018-03-16 10:13:54 -0700

    CALS-5651

---
+++ api-core-abac/src/main/java/gov/ca/cwds/authorizer/PlacementHomeReadAuthorizer.java
@@ -0,0 +1,18 @@
+package gov.ca.cwds.authorizer;
+
+import gov.ca.cwds.security.authorizer.StaticAuthorizer;
+import gov.ca.cwds.security.realm.PerryAccount;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.authz.permission.WildcardPermission;
+
+public class PlacementHomeReadAuthorizer implements StaticAuthorizer {
+  public static final String PLACEMENT_HOME_READ_PERMISSION = "placementHome:read";
+
+  @Override
+  public void authorize(PerryAccount perryAccount, SimpleAuthorizationInfo simpleAuthorizationInfo) {
+    if(perryAccount.getPrivileges().stream()
+        .anyMatch(priv-> priv.equals("Resource Management") || priv.equals("CWS Case Management"))) {
+      simpleAuthorizationInfo.addObjectPermission(new WildcardPermission(PLACEMENT_HOME_READ_PERMISSION));
+    }
+  }
+}
