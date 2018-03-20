a43c0269db622a5d35741fdd8fa5e8c5ef22c971
Volodymyr Petrusha <volodymyr.petrusha@osi.ca.gov>
2018-03-20 10:52:42 -0700

    Code climate fix

--- api-core-abac/src/main/java/gov/ca/cwds/authorizer/PlacementHomeReadAuthorizer.java
+++ api-core-abac/src/main/java/gov/ca/cwds/authorizer/PlacementHomeReadAuthorizer.java
@@ -6,13 +6,17 @@ import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.authz.permission.WildcardPermission;
 
 public class PlacementHomeReadAuthorizer implements StaticAuthorizer {
+
   public static final String PLACEMENT_HOME_READ_PERMISSION = "placementHome:read";
 
   @Override
-  public void authorize(PerryAccount perryAccount, SimpleAuthorizationInfo simpleAuthorizationInfo) {
-    if(perryAccount.getPrivileges().stream()
-        .anyMatch(priv-> priv.equals("Resource Management") || priv.equals("CWS Case Management System"))) {
-      simpleAuthorizationInfo.addObjectPermission(new WildcardPermission(PLACEMENT_HOME_READ_PERMISSION));
+  public void authorize(PerryAccount perryAccount,
+      SimpleAuthorizationInfo simpleAuthorizationInfo) {
+    if (perryAccount.getPrivileges().stream()
+        .anyMatch(priv -> priv.equals("Resource Management")
+            || priv.equals("CWS Case Management System"))) {
+      simpleAuthorizationInfo.addObjectPermission(
+          new WildcardPermission(PLACEMENT_HOME_READ_PERMISSION));
     }
   }
 }
