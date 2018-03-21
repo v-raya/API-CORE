a43c73aa49046fb8f96a9d9ff14fcd4630fdf925
Dmytro Palczewski <dmytro.palczewski@HHSTechGroup.com>
2018-03-21 12:00:25 -0700

    test rule is restored

--- legacy-data-access/src/test/java/gov/ca/cwds/data/legacy/cms/persistence/BaseCwsCmsInMemoryPersistenceTest.java
+++ legacy-data-access/src/test/java/gov/ca/cwds/data/legacy/cms/persistence/BaseCwsCmsInMemoryPersistenceTest.java
@@ -47,6 +47,9 @@ public abstract class BaseCwsCmsInMemoryPersistenceTest  {
   private static final Logger LOGGER = LoggerFactory
       .getLogger(gov.ca.cwds.data.legacy.cms.persistence.BaseCwsCmsInMemoryPersistenceTest.class);
 
+  @ClassRule
+  public static final InMemoryTestResources inMemoryTestResources = InMemoryTestResources.getInstance();
+
   @Before
   public void superBefore() throws Exception {
 //    PowerMockito.mockStatic(PrincipalUtils.class);
