74d6065222a43bdbf65fcd88c0f8665c984ae04a
sorych <dmytro.sorych@osi.ca.gov>
2018-03-21 15:23:54 -0700

    NEW:CASE-1399 Rule R05786 implemented

---
+++ legacy-data-access-services/src/main/java/gov/ca/cwds/cms/data/access/dto/OtherClientNameDTO.java
@@ -0,0 +1,67 @@
+package gov.ca.cwds.cms.data.access.dto;
+
+public class OtherClientNameDTO {
+  private String firstName;
+  private String lastName;
+  private String middleName;
+  private String namePrefixDescription;
+  private String suffixTitleDescription;
+  private String clientId;
+  private short nameType;
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getMiddleName() {
+    return middleName;
+  }
+
+  public void setMiddleName(String middleName) {
+    this.middleName = middleName;
+  }
+
+  public String getNamePrefixDescription() {
+    return namePrefixDescription;
+  }
+
+  public void setNamePrefixDescription(String namePrefixDescription) {
+    this.namePrefixDescription = namePrefixDescription;
+  }
+
+  public String getSuffixTitleDescription() {
+    return suffixTitleDescription;
+  }
+
+  public void setSuffixTitleDescription(String suffixTitleDescription) {
+    this.suffixTitleDescription = suffixTitleDescription;
+  }
+
+  public String getClientId() {
+    return clientId;
+  }
+
+  public void setClientId(String clientId) {
+    this.clientId = clientId;
+  }
+
+  public short getNameType() {
+    return nameType;
+  }
+
+  public void setNameType(short nameType) {
+    this.nameType = nameType;
+  }
+}
