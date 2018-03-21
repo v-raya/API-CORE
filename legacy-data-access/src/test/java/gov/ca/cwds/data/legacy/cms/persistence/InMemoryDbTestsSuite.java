ad898ecb6eae6f5e1836751992a68f5d9d297a83
Dmytro Palczewski <dmytro.palczewski@HHSTechGroup.com>
2018-03-21 10:41:00 -0700

    tests are added to test suite

--- legacy-data-access/src/test/java/gov/ca/cwds/data/legacy/cms/persistence/InMemoryDbTestsSuite.java
+++ legacy-data-access/src/test/java/gov/ca/cwds/data/legacy/cms/persistence/InMemoryDbTestsSuite.java
@@ -4,6 +4,23 @@ import gov.ca.cwds.data.legacy.cms.dao.CaseDaoTest;
 import gov.ca.cwds.data.legacy.cms.dao.ClientDaoTest;
 import gov.ca.cwds.data.legacy.cms.dao.ClientOtherEthnicitiesTest;
 import gov.ca.cwds.data.legacy.cms.dao.ClientRelationshipDaoTest;
+import gov.ca.cwds.data.legacy.cms.dao.CreditReportHistoryDaoTest;
+import gov.ca.cwds.data.legacy.cms.dao.CsecHistoryDaoTest;
+import gov.ca.cwds.data.legacy.cms.dao.DasHistoryDaoTest;
+import gov.ca.cwds.data.legacy.cms.dao.DeliveredServiceDaoTest;
+import gov.ca.cwds.data.legacy.cms.dao.FCEligibilityDaoTest;
+import gov.ca.cwds.data.legacy.cms.dao.HealthInterventionPlanDaoTest;
+import gov.ca.cwds.data.legacy.cms.dao.HealthReferralDaoTest;
+import gov.ca.cwds.data.legacy.cms.dao.HealthScreeningDaoTest;
+import gov.ca.cwds.data.legacy.cms.dao.MedicalEligibilityApplicationDaoTest;
+import gov.ca.cwds.data.legacy.cms.dao.NearFatalityDaoTest;
+import gov.ca.cwds.data.legacy.cms.dao.ParentalRightsTerminationDaoTest;
+import gov.ca.cwds.data.legacy.cms.dao.PaternityDetailDaoTest;
+import gov.ca.cwds.data.legacy.cms.dao.ReferralDaoTest;
+import gov.ca.cwds.data.legacy.cms.dao.SafetyAlertDaoTest;
+import gov.ca.cwds.data.legacy.cms.dao.SchoolOriginHistoryDaoTest;
+import gov.ca.cwds.data.legacy.cms.dao.SpecialEducationDaoTest;
+import gov.ca.cwds.data.legacy.cms.dao.TribalAncestryNotificationDaoTest;
 import org.junit.ClassRule;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -13,7 +30,24 @@ import org.junit.runners.Suite;
     CaseDaoTest.class,
     ClientDaoTest.class,
     ClientOtherEthnicitiesTest.class,
-    ClientRelationshipDaoTest.class
+    ClientRelationshipDaoTest.class,
+    CreditReportHistoryDaoTest.class,
+    CsecHistoryDaoTest.class,
+    DasHistoryDaoTest.class,
+    DeliveredServiceDaoTest.class,
+    FCEligibilityDaoTest.class,
+    HealthInterventionPlanDaoTest.class,
+    HealthReferralDaoTest.class,
+    HealthScreeningDaoTest.class,
+    MedicalEligibilityApplicationDaoTest.class,
+    NearFatalityDaoTest.class,
+    ParentalRightsTerminationDaoTest.class,
+    PaternityDetailDaoTest.class,
+    ReferralDaoTest.class,
+    SafetyAlertDaoTest.class,
+    SchoolOriginHistoryDaoTest.class,
+    SpecialEducationDaoTest.class,
+    TribalAncestryNotificationDaoTest.class
 })
 public class InMemoryDbTestsSuite {
   @ClassRule
