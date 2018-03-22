3d6fdcff42d4711701c3d8dca97f95426acee0e5
Adarsh Vandana <adarsh.vandana@osi.ca.gov>
2018-03-22 12:50:51 -0700

    Removed the password from constant and cleaned up client moving it into small methods.

--- api-core-test-support/src/main/java/gov/ca/cwds/rest/authenticate/CWDSAuthenticationClientConfig.java
+++ api-core-test-support/src/main/java/gov/ca/cwds/rest/authenticate/CWDSAuthenticationClientConfig.java
@@ -8,7 +8,7 @@ import java.util.Map;
  */
 public class CWDSAuthenticationClientConfig {
 
-  class TokenCredentials {
+  static class TokenCredentials {
 
     public TokenCredentials() {
       // no-opt
