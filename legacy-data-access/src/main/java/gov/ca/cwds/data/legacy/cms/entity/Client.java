849325994e4acbc2bcfb993e9af4f9600500e6f8
Mykhailo <m.zemlyanoi@gmail.com>
2018-03-27 22:34:20 -0700

    RL-16 ( The start date cannot be less than the birth date (if it exists) of either the Primary or Secondary Client. This rule only applies when both the primary and secondary Clients have birth dates entered)
    - drl file
    - test

--- legacy-data-access/src/main/java/gov/ca/cwds/data/legacy/cms/entity/Client.java
+++ legacy-data-access/src/main/java/gov/ca/cwds/data/legacy/cms/entity/Client.java
@@ -33,7 +33,6 @@ import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.OrderBy;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
