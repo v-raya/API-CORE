6e318681a3be0b77f7e63937e219183071e8601d
Ievgenii Drozd <justajavaboy@users.noreply.github.com>
2018-04-02 14:38:22 -0700

    fixed errors

--- legacy-data-access/src/main/java/gov/ca/cwds/data/legacy/cms/entity/Referral.java
+++ legacy-data-access/src/main/java/gov/ca/cwds/data/legacy/cms/entity/Referral.java
@@ -66,7 +66,7 @@ import org.hibernate.annotations.Type;
     @NamedQuery(
         name = Referral.FIND_BY_CLIENT,
         query =
-            "SELECT DISTINCT r.referral FROM ReferralClient r LEFT JOIN r.referral WHERE r.client.identifier=:clientId"
+            "SELECT DISTINCT r.referral FROM gov.ca.cwds.data.legacy.cms.entity.ReferralClient r LEFT JOIN r.referral WHERE r.client.identifier=:clientId"
     )
 })
 @SuppressWarnings("squid:S3437")
