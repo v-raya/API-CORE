f3f57d0da351675df894b6265364cd83636deeda
Mykhailo <m.zemlyanoi@gmail.com>
2018-04-08 19:51:44 -0700

    Tests
    - AuthenticationUtilsTest

---
+++ api-core-test-support/src/test/java/gov/ca/cwds/rest/authenticate/AuthenticationUtilsTest.java
@@ -0,0 +1,33 @@
+package gov.ca.cwds.rest.authenticate;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author CWDS TPT-3 Team
+ */
+public class AuthenticationUtilsTest {
+
+  private AuthenticationUtils authenticationUtils;
+
+  @Before
+  public void init() {
+    authenticationUtils = new AuthenticationUtils();
+  }
+
+  @Test
+  public void getToken() throws Exception {
+    String token = authenticationUtils.getToken(UserGroup.COUNTY_SEALED);
+    assertNotNull(token);
+    token = authenticationUtils.getToken(UserGroup.COUNTY_SENSITIVE);
+    assertNotNull(token);
+    token = authenticationUtils.getToken(UserGroup.SOCIAL_WORKER);
+    assertNotNull(token);
+    token = authenticationUtils.getToken(UserGroup.STATE_SEALED);
+    assertNotNull(token);
+    token = authenticationUtils.getToken(UserGroup.STATE_SENSITIVE);
+    assertNotNull(token);
+  }
+}
