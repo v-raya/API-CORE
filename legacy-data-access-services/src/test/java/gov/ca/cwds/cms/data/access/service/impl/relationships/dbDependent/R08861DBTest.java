8f27e383448e046d2f26c8eeeb6b4e7868cd9224
Mykhailo <m.zemlyanoi@gmail.com>
2018-04-17 09:41:39 -0700

    RL-52
     - Add DB Integration test

--- legacy-data-access-services/src/test/java/gov/ca/cwds/cms/data/access/service/impl/relationships/dbDependent/R08861DBTest.java
+++ legacy-data-access-services/src/test/java/gov/ca/cwds/cms/data/access/service/impl/relationships/dbDependent/R08861DBTest.java
@@ -3,8 +3,7 @@ package gov.ca.cwds.cms.data.access.service.impl.relationships.dbDependent;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.powermock.api.mockito.PowerMockito.mockStatic;
-import static org.powermock.api.mockito.PowerMockito.when;
+import static org.junit.Assert.assertTrue;
 
 import gov.ca.cwds.cms.data.access.dto.ClientRelationshipAwareDTO;
 import gov.ca.cwds.cms.data.access.service.BusinessValidationService;
@@ -18,10 +17,9 @@ import gov.ca.cwds.data.legacy.cms.entity.ClientRelationship;
 import gov.ca.cwds.data.legacy.cms.entity.TribalMembershipVerification;
 import gov.ca.cwds.data.legacy.cms.entity.syscodes.ClientRelationshipType;
 import gov.ca.cwds.drools.DroolsService;
-import gov.ca.cwds.security.realm.PerryAccount;
-import gov.ca.cwds.security.utils.PrincipalUtils;
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.collections.CollectionUtils;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -31,6 +29,8 @@ public class R08861DBTest extends BaseCwsCmsInMemoryPersistenceTest {
   public static final String CLIENT_ID = "AJ4piWo0WL";
   public static final String PARENT_CLIENT_ID = "AJ4piWo0WP";
   public static final String RELATIONSHIP_ID = "Aaqj06L00h";
+  public static final String PARENT_CLIENT_ID_2 = "AJ4piWo0qq";
+  public static final String RELATIONSHIP_ID_2 = "Aaqj06L0ww";
 
   private ClientDao clientDao;
   private TribalMembershipVerificationDao tribalMembershipVerificationDao;
@@ -39,7 +39,7 @@ public class R08861DBTest extends BaseCwsCmsInMemoryPersistenceTest {
   private BusinessValidationService businessValidationService;
 
   @Before
-  public void before() {
+  public void before() throws Exception {
     businessValidationService = new BusinessValidationService(new DroolsService());
     tribalMembershipVerificationDao = new TribalMembershipVerificationDao(sessionFactory);
     clientRelationshipDao = new ClientRelationshipDao(sessionFactory);
@@ -50,60 +50,78 @@ public class R08861DBTest extends BaseCwsCmsInMemoryPersistenceTest {
             businessValidationService,
             clientDao,
             tribalMembershipVerificationDao);
+
+    cleanAllAndInsert("/dbunit/R08861.xml");
   }
 
   @Test
   public void testNeedDeleteSubTribals() throws Exception {
-    cleanAllAndInsert("/dbunit/R08861.xml");
 
-    final List<TribalMembershipVerification> shouldBeDeleted = new ArrayList<>();
-    final List<TribalMembershipVerification> afterDelete = new ArrayList<>();
+    final List<TribalMembershipVerification> shouldBeDeleted =
+        getTrbalMembershipVerificationThatHaveSubTribals(CLIENT_ID, PARENT_CLIENT_ID);
 
-    executeInTransaction(
-        sessionFactory,
-        (sessionFactory) -> {
-          try {
-            ClientRelationship relationship = clientRelationshipCoreService.find(RELATIONSHIP_ID);
-            ClientRelationshipType type = new ClientRelationshipType();
-            type.setSystemId((short) 291);
-            relationship.setType(type);
+    updateClientRelationship(RELATIONSHIP_ID);
 
-            shouldBeDeleted.addAll(tribalMembershipVerificationDao.findTribalsThatHaveSubTribalsByClientId(
-                    CLIENT_ID, PARENT_CLIENT_ID));
-
-            ClientRelationshipAwareDTO awareDTO = new ClientRelationshipAwareDTO();
-            awareDTO.setEntity(relationship);
-
-            clientRelationshipCoreService.update(awareDTO);
-          } catch (DataAccessServicesException e) {
-            e.printStackTrace();
-          }
-        });
-
-    executeInTransaction(
-        sessionFactory,
-        (sessionFactory) -> {
-          afterDelete.addAll(tribalMembershipVerificationDao.findTribalsThatHaveSubTribalsByClientId(
-              CLIENT_ID, PARENT_CLIENT_ID));
-        });
+    final List<TribalMembershipVerification> afterDelete =
+        getTrbalMembershipVerificationThatHaveSubTribals(CLIENT_ID, PARENT_CLIENT_ID);
 
     assertNotEquals(shouldBeDeleted, afterDelete);
     assertNotNull(afterDelete);
     assertEquals(1, afterDelete.size());
+  }
 
-    final List<TribalMembershipVerification> afterDelete2 = new ArrayList<>();
+  @Test
+  public void testDoNotNeedDeleteSubTribals() throws Exception {
+
+    final List<TribalMembershipVerification> shouldBeDeleted =
+        getTrbalMembershipVerificationThatHaveSubTribals(CLIENT_ID, PARENT_CLIENT_ID_2);
+
+    updateClientRelationship(RELATIONSHIP_ID_2);
+
+    final List<TribalMembershipVerification> afterDelete =
+        getTrbalMembershipVerificationThatHaveSubTribals(CLIENT_ID, PARENT_CLIENT_ID_2);
+
+    assertNotNull(afterDelete);
+    assertNotNull(shouldBeDeleted);
+    assertTrue(CollectionUtils.isEmpty(shouldBeDeleted));
+    assertTrue(CollectionUtils.isEmpty(afterDelete));
+
+    final List<TribalMembershipVerification> shouldBeDeleted2 =
+        getTrbalMembershipVerificationThatHaveSubTribals(CLIENT_ID, PARENT_CLIENT_ID_2);
+
+    updateClientRelationship(RELATIONSHIP_ID_2);
+
+    final List<TribalMembershipVerification> afterDelete2 =
+        getTrbalMembershipVerificationThatHaveSubTribals(CLIENT_ID, PARENT_CLIENT_ID_2);
+
+    assertNotNull(afterDelete);
+    assertNotNull(shouldBeDeleted);
+    assertTrue(CollectionUtils.isEmpty(shouldBeDeleted));
+    assertTrue(CollectionUtils.isEmpty(afterDelete));
+  }
+
+  private List<TribalMembershipVerification> getTrbalMembershipVerificationThatHaveSubTribals(String clientId, String parentId) {
+    final List<TribalMembershipVerification> shouldBeDeleted = new ArrayList<>();
 
     executeInTransaction(
         sessionFactory,
         (sessionFactory) -> {
+          shouldBeDeleted.addAll(
+              tribalMembershipVerificationDao.findTribalsThatHaveSubTribalsByClientId(
+                  clientId, parentId));
+        });
+    return shouldBeDeleted;
+  }
+
+  private void updateClientRelationship(String relationshipId) {
+    executeInTransaction(
+        sessionFactory,
+        (sessionFactory) -> {
           try {
-            ClientRelationship relationship = clientRelationshipCoreService.find(RELATIONSHIP_ID);
+            ClientRelationship relationship = clientRelationshipCoreService.find(relationshipId);
             ClientRelationshipType type = new ClientRelationshipType();
             type.setSystemId((short) 291);
             relationship.setType(type);
-
-            shouldBeDeleted.addAll(tribalMembershipVerificationDao.findTribalsThatHaveSubTribalsByClientId(
-                CLIENT_ID, PARENT_CLIENT_ID));
 
             ClientRelationshipAwareDTO awareDTO = new ClientRelationshipAwareDTO();
             awareDTO.setEntity(relationship);
@@ -113,31 +131,5 @@ public class R08861DBTest extends BaseCwsCmsInMemoryPersistenceTest {
             e.printStackTrace();
           }
         });
-
-    executeInTransaction(
-        sessionFactory,
-        (sessionFactory) -> {
-          afterDelete2.addAll(tribalMembershipVerificationDao.findTribalsThatHaveSubTribalsByClientId(
-              CLIENT_ID, PARENT_CLIENT_ID));
-        });
-
-    executeInTransaction(
-        sessionFactory,
-        (sessionFactory) -> {
-          afterDelete.addAll(tribalMembershipVerificationDao.findTribalsThatHaveSubTribalsByClientId(
-              CLIENT_ID, PARENT_CLIENT_ID));
-        });
-
-    assertNotEquals(afterDelete2, afterDelete);
-    assertNotNull(afterDelete2);
-    assertEquals(1, afterDelete2.size());
-  }
-
-  private void initUserAccount(String countyCwsCode) {
-    PerryAccount perryAccount = new PerryAccount();
-    perryAccount.setCountyCwsCode(countyCwsCode);
-    mockStatic(PrincipalUtils.class);
-    when(PrincipalUtils.getPrincipal()).thenReturn(perryAccount);
-    when(PrincipalUtils.getStaffPersonId()).thenReturn("ox5");
   }
 }
