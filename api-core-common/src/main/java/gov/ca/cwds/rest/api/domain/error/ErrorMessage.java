23f25ccbff233f0fe0d9c85d98cccf2ad4e63cf4
David R. Smith <David.Smith@osi.ca.gov>
2018-05-15 15:38:22 -0700

    SNAP-49: Production Environment no HOI shows for client

--- api-core-common/src/main/java/gov/ca/cwds/rest/api/domain/error/ErrorMessage.java
+++ api-core-common/src/main/java/gov/ca/cwds/rest/api/domain/error/ErrorMessage.java
@@ -1,47 +1,51 @@
 package gov.ca.cwds.rest.api.domain.error;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 import gov.ca.cwds.rest.serializer.ErrorMessageSerializer;
 
 @JsonSerialize(using = ErrorMessageSerializer.class)
 public class ErrorMessage {
-    private final ErrorType type;
-    private final String message;
-    private final String source;
+  private final ErrorType type;
+  private final String message;
+  private final String source;
 
 
-    public ErrorType getType() {
-        return type;
+  public ErrorType getType() {
+    return type;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public String getSource() {
+    return source;
+  }
+
+  public ErrorMessage(ErrorType type, String message, String source) {
+    this.type = type;
+    this.message = message;
+    this.source = source;
+  }
+
+  public static enum ErrorType {
+    //@formatter:off
+    CLIENT_CONTRACT("Client Contract Error"), 
+    VALIDATION("Validation Error"), 
+    BUSINESS("Buiness Rules"), 
+    DATA_ACCESS("Data Access");
+    //@formatter:on 
+
+    private final String label;
+
+    private ErrorType(String label) {
+      this.label = label;
     }
 
-    public String getMessage() {
-        return message;
+    public String getLabel() {
+      return label;
     }
+  }
 
-    public String getSource() {
-        return source;
-    }
-
-    public ErrorMessage(ErrorType type, String message, String source){
-        this.type = type;
-        this.message = message;
-        this.source = source;
-    }
-
-    public static enum ErrorType {
-        CLIENT_CONTRACT("Client Contract Error"),
-        VALIDATION("Validation Error"),
-        BUSINESS("Buiness Rules"),
-        DATA_ACCESS("Data Access");
-
-        private final String label;
-
-        private ErrorType(String label) {
-            this.label = label;
-        }
-
-        public String getLabel() {
-            return label;
-        }
-    }
 }
