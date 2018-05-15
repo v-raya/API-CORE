f743fec67ec58ecd4b0e2e26db70c9ff60d99c88
David R. Smith <David.Smith@osi.ca.gov>
2018-05-15 16:36:44 -0700

    SNAP-49: Production: no HOI shown for client

--- api-core-common/src/main/java/gov/ca/cwds/rest/api/domain/error/ErrorMessage.java
+++ api-core-common/src/main/java/gov/ca/cwds/rest/api/domain/error/ErrorMessage.java
@@ -1,15 +1,22 @@
 package gov.ca.cwds.rest.api.domain.error;
 
+import org.slf4j.event.Level;
+
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 import gov.ca.cwds.rest.serializer.ErrorMessageSerializer;
 
+/**
+ * JSON-aware error message, used to transmit errors from services back to the domain response.
+ * 
+ * @author CWDS API Team
+ */
 @JsonSerialize(using = ErrorMessageSerializer.class)
 public class ErrorMessage {
+
   private final ErrorType type;
   private final String message;
   private final String source;
-
 
   public ErrorType getType() {
     return type;
@@ -30,21 +37,43 @@ public class ErrorMessage {
   }
 
   public static enum ErrorType {
+
     //@formatter:off
+    CLIENT_AUTHORIZATION("Client Authorization Error"), 
+
+    /**
+     * User is not authorized to view a client, typically in passing, such as a child's half-sister's foster sibling. Show a warning, not an error.
+     */
+    CLIENT_AUTHORIZATION_WARNING("Client Authorization Warning", Level.WARN), 
+    
     CLIENT_CONTRACT("Client Contract Error"), 
+    
     VALIDATION("Validation Error"), 
+    
     BUSINESS("Buiness Rules"), 
+    
     DATA_ACCESS("Data Access");
     //@formatter:on 
 
     private final String label;
+    private final Level level;
 
     private ErrorType(String label) {
       this.label = label;
+      this.level = Level.ERROR;
+    }
+
+    private ErrorType(String label, Level level) {
+      this.label = label;
+      this.level = level;
     }
 
     public String getLabel() {
       return label;
+    }
+
+    public Level getLevel() {
+      return level;
     }
   }
 
