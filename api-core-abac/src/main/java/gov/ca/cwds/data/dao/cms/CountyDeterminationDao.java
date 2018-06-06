a75b404989c5dc7eea237d7f2f6e3a5592371a74
Volodymyr Petrusha <volodymyr.petrusha@osi.ca.gov>
2018-06-06 15:19:15 -0700

    Made "managed" property optional

--- api-core-abac/src/main/java/gov/ca/cwds/data/dao/cms/CountyDeterminationDao.java
+++ api-core-abac/src/main/java/gov/ca/cwds/data/dao/cms/CountyDeterminationDao.java
@@ -1,16 +1,12 @@
 package gov.ca.cwds.data.dao.cms;
 
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+import gov.ca.cwds.inject.CwsRsSessionFactory;
 import java.util.List;
-
-import org.apache.commons.lang3.StringUtils;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.context.internal.ManagedSessionContext;
-
-import com.google.inject.Inject;
-import com.google.inject.name.Named;
-
-import gov.ca.cwds.inject.CwsRsSessionFactory;
 
 /**
  * Hibernate DAO for getting county of client from the {@code CLIENT_CNTY} table in a DB2
@@ -23,13 +19,13 @@ public class CountyDeterminationDao extends BaseAuthorizationDao {
   private static final String CLIENT_COUNTY_CACHE = "clientCountyCache";
   private static final String NQ_PARAM_CLIENT_ID = "clientId";
 
-  protected final boolean managed;
+  @Inject(optional = true)
+  @Named("managed")
+  private String managedProp;
 
   @Inject
-  public CountyDeterminationDao(@CwsRsSessionFactory SessionFactory sessionFactory,
-      @Named("managed") String managed) {
+  public CountyDeterminationDao(@CwsRsSessionFactory SessionFactory sessionFactory) {
     super(sessionFactory);
-    this.managed = StringUtils.isBlank(managed) || !"Y".equals(managed);
   }
 
   /**
@@ -46,6 +42,7 @@ public class CountyDeterminationDao extends BaseAuthorizationDao {
 
   @SuppressWarnings("unchecked")
   private List<Short> executeNativeQuery(final String namedQuery, final String clientId) {
+    final boolean managed = !"Y".equals(managedProp);
     Session session = null;
     try {
       session = grabSession();
