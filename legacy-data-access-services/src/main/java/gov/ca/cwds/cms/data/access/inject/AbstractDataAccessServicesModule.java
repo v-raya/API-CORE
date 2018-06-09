b7232707e6d2e882a838c0e5435bb74a88c8fdcc
Leonid Marushevskyi <leonid.marushevskiy@osi.ca.gov>
2018-06-08 23:00:58 -0700

    Merge branch 'master' into AARA-226
    # Conflicts:
    #	legacy-data-access-services/src/main/java/gov/ca/cwds/cms/data/access/inject/AbstractDataAccessServicesModule.java

--- legacy-data-access-services/src/main/java/gov/ca/cwds/cms/data/access/inject/AbstractDataAccessServicesModule.java
+++ legacy-data-access-services/src/main/java/gov/ca/cwds/cms/data/access/inject/AbstractDataAccessServicesModule.java
@@ -1,17 +1,9 @@
 package gov.ca.cwds.cms.data.access.inject;
 
-import gov.ca.cwds.cms.data.access.mapper.ClientMapper;
-import gov.ca.cwds.cms.data.access.service.impl.CountyLicenseCaseService;
-import gov.ca.cwds.cms.data.access.service.impl.StaffPersonService;
-import gov.ca.cwds.cms.data.access.service.impl.clientrelationship.ClientRelationshipCoreService;
-import gov.ca.cwds.cms.data.access.service.impl.tribalmembership.TribalMembershipVerificationCoreService;
-import org.hibernate.SessionFactory;
-
 import com.google.inject.AbstractModule;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.google.inject.Provides;
-
 import gov.ca.cwds.cms.data.access.mapper.ClientMapper;
 import gov.ca.cwds.cms.data.access.mapper.CountyOwnershipMapper;
 import gov.ca.cwds.cms.data.access.mapper.ExternalInterfaceMapper;
@@ -19,11 +11,14 @@ import gov.ca.cwds.cms.data.access.service.BusinessValidationService;
 import gov.ca.cwds.cms.data.access.service.ClientRelationshipService;
 import gov.ca.cwds.cms.data.access.service.impl.ChildClientCoreService;
 import gov.ca.cwds.cms.data.access.service.impl.ClientCoreService;
+import gov.ca.cwds.cms.data.access.service.impl.CountyLicenseCaseService;
 import gov.ca.cwds.cms.data.access.service.impl.PlacementHomeCoreService;
+import gov.ca.cwds.cms.data.access.service.impl.StaffPersonService;
 import gov.ca.cwds.cms.data.access.service.impl.SubstituteCareProviderCoreService;
 import gov.ca.cwds.cms.data.access.service.impl.clientrelationship.ClientRelationshipCoreService;
 import gov.ca.cwds.cms.data.access.service.impl.tribalmembership.CreateLifeCycle;
 import gov.ca.cwds.cms.data.access.service.impl.tribalmembership.TribalMembershipVerificationCoreService;
+import org.hibernate.SessionFactory;
 
 /**
  * Common module binds services for authorization, common client services, and singleton instances.
