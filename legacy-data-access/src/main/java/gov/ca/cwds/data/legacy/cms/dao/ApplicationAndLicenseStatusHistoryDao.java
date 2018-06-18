010bc7706e2a953085f654c9f35a577545cc79e9
DmitriyRudenko <Dmitry.Rudenko@osi.ca.gov>
2018-06-18 15:04:21 -0700

    ApplicationAndLicenseStatusHistory entity

---
+++ legacy-data-access/src/main/java/gov/ca/cwds/data/legacy/cms/dao/ApplicationAndLicenseStatusHistoryDao.java
@@ -0,0 +1,19 @@
+package gov.ca.cwds.data.legacy.cms.dao;
+
+import com.google.inject.Inject;
+import gov.ca.cwds.data.BaseDaoImpl;
+import gov.ca.cwds.data.legacy.cms.entity.ApplicationAndLicenseStatusHistory;
+import gov.ca.cwds.inject.CmsSessionFactory;
+import org.hibernate.SessionFactory;
+
+public class ApplicationAndLicenseStatusHistoryDao extends BaseDaoImpl<ApplicationAndLicenseStatusHistory> {
+  /**
+   * Constructor
+   *
+   * @param sessionFactory The session factory
+   */
+  @Inject
+  public ApplicationAndLicenseStatusHistoryDao(@CmsSessionFactory SessionFactory sessionFactory) {
+    super(sessionFactory);
+  }
+}
