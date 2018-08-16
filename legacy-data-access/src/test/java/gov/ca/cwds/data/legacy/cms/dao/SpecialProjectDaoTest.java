5839d2c8c59c06f1fbf2ef298b05960b92b9f789
David R. Smith <David.Smith@osi.ca.gov>
2018-08-16 10:20:53 -0700

    HOT-2191: CMS key generation

--- legacy-data-access/src/test/java/gov/ca/cwds/data/legacy/cms/dao/SpecialProjectDaoTest.java
+++ legacy-data-access/src/test/java/gov/ca/cwds/data/legacy/cms/dao/SpecialProjectDaoTest.java
@@ -5,16 +5,24 @@ import java.util.List;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import gov.ca.cwds.data.legacy.cms.entity.SpecialProject;
 import gov.ca.cwds.data.legacy.cms.persistence.BaseCwsCmsInMemoryPersistenceTest;
+import gov.ca.cwds.security.utils.PrincipalUtils;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({PrincipalUtils.class})
 public class SpecialProjectDaoTest extends BaseCwsCmsInMemoryPersistenceTest {
 
   private SpecialProjectDao dao = null;
 
+  @Override
   @Before
-  public void before() {
+  public void setup() throws Exception {
+    super.setup();
     dao = new SpecialProjectDao(sessionFactory);
   }
 
@@ -23,10 +31,10 @@ public class SpecialProjectDaoTest extends BaseCwsCmsInMemoryPersistenceTest {
     cleanAllAndInsert("/dbunit/SpecialProjects.xml");
 
     executeInTransaction(sessionFactory, sessionFactory -> {
-      List<SpecialProject> specialProjects =
+      final List<SpecialProject> specialProjects =
           dao.findActiveSafelySurrenderedBabiesSpecialProjectByGovernmentEntity((short) 1086);
       Assert.assertEquals(1, specialProjects.size());
-      SpecialProject specialProject = specialProjects.get(0);
+      final SpecialProject specialProject = specialProjects.get(0);
 
       Assert.assertEquals("S-Safely Surrendered Baby", specialProject.getName());
       Assert.assertEquals("Safely Surrendered Baby", specialProject.getProjectDescription());
@@ -34,20 +42,22 @@ public class SpecialProjectDaoTest extends BaseCwsCmsInMemoryPersistenceTest {
       Assert.assertEquals(Boolean.FALSE, specialProject.getArrchiveAssociationIndicator());
     });
   }
-  
+
   @Test
   public void testFindByReferralIdAndSpecialProjectId() throws Exception {
     cleanAllAndInsert("/dbunit/SpecialProjects.xml");
 
     executeInTransaction(sessionFactory, sessionFactor -> {
-      List<SpecialProject> specialProjects = dao.findSpecialProjectByGovernmentEntityAndName("S-CSEC Referral", (short) 1086);
-      Assert.assertEquals(1,  specialProjects.size());
-      SpecialProject specialProject = specialProjects.get(0);
-      
+      final List<SpecialProject> specialProjects =
+          dao.findSpecialProjectByGovernmentEntityAndName("S-CSEC Referral", (short) 1086);
+      Assert.assertEquals(1, specialProjects.size());
+      final SpecialProject specialProject = specialProjects.get(0);
+
       Assert.assertEquals("S-CSEC Referral", specialProject.getName());
       Assert.assertEquals("CSEC special project referral", specialProject.getProjectDescription());
       Assert.assertEquals((short) 1086, specialProject.getGovernmentEntityType().shortValue());
       Assert.assertEquals(Boolean.FALSE, specialProject.getArrchiveAssociationIndicator());
     });
   }
+
 }
