c9c0ef4f156e78258eda5ad20a642c9c59583582
DmitriyRudenko <Dmitry.Rudenko@osi.ca.gov>
2018-11-15 16:43:46 -0800

    H2 timestamp alias

--- api-core-test-support/src/main/java/gov/ca/cwds/test/support/H2Function.java
+++ api-core-test-support/src/main/java/gov/ca/cwds/test/support/H2Function.java
@@ -8,13 +8,14 @@ import java.time.LocalDateTime;
 public class H2Function {
 
   public static void createTimestampAlias(final Connection connection) throws SQLException {
-    Statement statement = connection.createStatement();
-    statement.execute("CREATE ALIAS TIMESTAMP "
-      + "FOR \"gov.ca.cwds.test.support.H2Function.timestampFunction\" ");
-    statement.close();
+    try (Statement statement = connection.createStatement()) {
+      statement.execute("CREATE ALIAS TIMESTAMP "
+        + "FOR \"gov.ca.cwds.test.support.H2Function.timestampFunction\" ");
+    }
   }
 
-  public static LocalDateTime timestampFunction(final java.sql.Date date, final java.sql.Time time) {
+  public static LocalDateTime timestampFunction(final java.sql.Date date,
+    final java.sql.Time time) {
     if (date == null || time == null) {
       return null;
     }
